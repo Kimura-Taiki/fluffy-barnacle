2dc1e50a01e701b6db69ef1fa5c188396e336703
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-12-26 18:01:36 +0900

    J003:transform系統はsurface系統と別枠

--- py.py
+++ py.py
@@ -6,12 +6,13 @@ WX = 800
 WY = 600
 FRAMES_PER_SECOND = 30
 
-img = pygame.image.load("cards/na_00_hajimari_a_n_1.png")   # 340x475
-
 pygame.init()
 pygame.display.set_caption("FFF")
 screen = pygame.display.set_mode((WX, WY))
 clock = pygame.time.Clock()
+
+img = pygame.image.load("cards/na_00_hajimari_a_n_1.png").convert_alpha()   # 340x475
+mini_img = pygame.transform.rotozoom(surface=img, angle=90.0, scale=0.4)     # 68x190
 
 def mainloop() -> None:
     for event in pygame.event.get():
@@ -19,7 +20,7 @@ def mainloop() -> None:
             pygame.quit()
             sys.exit()
     screen.fill(color=(255, 255, 128))
-    screen.blit(source=img, dest=[100, 100])
+    screen.blit(source=mini_img, dest=[100, 100])
     pygame.display.update()
     clock.tick(FRAMES_PER_SECOND)
 
