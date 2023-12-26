3f1035fcd9aafd326d627e74ec15ec0bda54181d
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-12-26 13:53:40 +0900

    J001:メインループ

--- py.py
+++ py.py
@@ -1,1 +1,23 @@
-print("hello word")
+import pygame
+from pygame.locals import QUIT
+import sys
+
+WX = 800
+WY = 600
+FRAMES_PER_SECOND = 30
+
+pygame.init()
+pygame.display.set_caption("FFF")
+screen = pygame.display.set_mode((WX, WY))
+clock = pygame.time.Clock()
+
+def mainloop() -> None:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+    clock.tick(FRAMES_PER_SECOND)
+
+while True:
+    mainloop()
