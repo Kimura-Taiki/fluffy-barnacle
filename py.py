edef71ab5b1146ca40e3d8a473c38fe47ca09fab
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-12-26 17:37:16 +0900

    J002:blitした

--- py.py
+++ py.py
@@ -6,6 +6,8 @@ WX = 800
 WY = 600
 FRAMES_PER_SECOND = 30
 
+img = pygame.image.load("cards/na_00_hajimari_a_n_1.png")   # 340x475
+
 pygame.init()
 pygame.display.set_caption("FFF")
 screen = pygame.display.set_mode((WX, WY))
@@ -16,6 +18,8 @@ def mainloop() -> None:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    screen.fill(color=(255, 255, 128))
+    screen.blit(source=img, dest=[100, 100])
     pygame.display.update()
     clock.tick(FRAMES_PER_SECOND)
 
