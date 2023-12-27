d6bd4f31f9793258d5b0a0613575a43a681829ba
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-12-27 12:29:40 +0900

    J006:mod.hudaへHudaクラスを分離

---
+++ mod/huda.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame.surface import Surface
+from math import sin, cos, radians
+
+class Huda():
+
+    def __init__(self, screen: Surface, img: Surface, angle: float=0.0, scale: float=0.4, x:int | float=0, y:int | float=0) -> None:
+        self.screen = screen
+        self.img_nega = img
+        self.img_rz = pygame.transform.rotozoom(surface=img, angle=angle, scale=scale)
+        self.angle = angle
+        self.scale = scale
+        self.x = int(x)
+        self.y = int(y)
+        self.vertices = [self.rotated_verticle(i[0], i[1]) for i in [[-170.0, -237.5], [170.0, -237.5], [170.0, 237.5], [-170.0, 237.5]]]
+        print(self.vertices)
+
+    def rotated_verticle(self, x:int | float, y:int | float) -> list[int]:
+        rad = radians(-self.angle)
+        return [int(self.x+(cos(rad)*x-sin(rad)*y)*self.scale), int(self.y+(sin(rad)*x+cos(rad)*y)*self.scale)]
+
+    def draw(self) -> None:
+        [pygame.draw.circle(surface=self.screen, color=(0, 0, 255), center=[i[0], i[1]], radius=10) for i in self.vertices]
+        self.screen.blit(source=self.img_rz, dest=[self.x-self.img_rz.get_width()/2, self.y-self.img_rz.get_height()/2])
+
+    @property
+    def dest(self) -> [int, int]:
+        return self.x, self.y
+    
+    @dest.setter
+    def dest(self, x:int | float, y:int | float) -> None:
+        self.x, self.y = int(x), int(y)
+
--- py.py
+++ py.py
@@ -1,7 +1,8 @@
 import pygame
 from pygame.locals import QUIT
 import sys
-from math import sin, cos, radians
+
+from mod.huda import Huda
 
 WX = 800
 WY = 600
@@ -11,41 +12,6 @@ pygame.init()
 pygame.display.set_caption("FFF")
 screen = pygame.display.set_mode((WX, WY))
 clock = pygame.time.Clock()
-
-img = pygame.image.load("cards/na_00_hajimari_a_n_1.png").convert_alpha()   # 340x475
-mini_img = pygame.transform.rotozoom(surface=img, angle=90.0, scale=0.4)     # 68x190
-
-class Huda():
-    from pygame.surface import Surface
-
-    coord_diff = [[-170.0, -237.5], [170.0, -237.5], [170.0, 237.5], [-170.0, 237.5]]
-
-    def __init__(self, screen: Surface, img: Surface, angle: float=0.0, scale: float=0.4, x:int | float=0, y:int | float=0) -> None:
-        self.screen = screen
-        self.img_nega = img
-        self.img_rz = pygame.transform.rotozoom(surface=img, angle=angle, scale=scale)
-        self.angle = angle
-        self.scale = scale
-        self.x = int(x)
-        self.y = int(y)
-        self.vertices = [self.rotated_verticle(i[0], i[1]) for i in [[-170.0, -237.5], [170.0, -237.5], [170.0, 237.5], [-170.0, 237.5]]]
-        print(self.vertices)
-
-    def rotated_verticle(self, x:int | float, y:int | float) -> list[int]:
-        rad = radians(-self.angle)
-        return [int(self.x+(cos(rad)*x-sin(rad)*y)*self.scale), int(self.y+(sin(rad)*x+cos(rad)*y)*self.scale)]
-
-    def draw(self) -> None:
-        [pygame.draw.circle(surface=self.screen, color=(0, 0, 255), center=[i[0], i[1]], radius=10) for i in self.vertices]
-        self.screen.blit(source=self.img_rz, dest=[self.x-self.img_rz.get_width()/2, self.y-self.img_rz.get_height()/2])
-
-    @property
-    def dest(self) -> [int, int]:
-        return self.x, self.y
-    
-    @dest.setter
-    def dest(self, x:int | float, y:int | float) -> None:
-        self.x, self.y = int(x), int(y)
 
 huda = Huda(screen=screen, img=pygame.image.load("cards/na_00_hajimari_a_n_1.png").convert_alpha(),
             angle=90.0, scale=0.4, x=100, y=100)
