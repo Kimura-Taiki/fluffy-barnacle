80041bacf64f22a8735cfa31ca0d1c36f4e5bbcf
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-12-27 12:17:08 +0900

    J005:Huda.verticlesを指定した

--- py.py
+++ py.py
@@ -28,14 +28,16 @@ class Huda():
         self.scale = scale
         self.x = int(x)
         self.y = int(y)
-        self.vertices = []
+        self.vertices = [self.rotated_verticle(i[0], i[1]) for i in [[-170.0, -237.5], [170.0, -237.5], [170.0, 237.5], [-170.0, 237.5]]]
+        print(self.vertices)
 
     def rotated_verticle(self, x:int | float, y:int | float) -> list[int]:
         rad = radians(-self.angle)
-        return int(self.x+cos(rad)*x-sin(rad)*y), int(self.y+sin(rad)*x+cos(rad)*y)
+        return [int(self.x+(cos(rad)*x-sin(rad)*y)*self.scale), int(self.y+(sin(rad)*x+cos(rad)*y)*self.scale)]
 
-    def draw_huda(self) -> None:
-        self.screen.blit(source=self.img_rz, dest=[self.x, self.y])
+    def draw(self) -> None:
+        [pygame.draw.circle(surface=self.screen, color=(0, 0, 255), center=[i[0], i[1]], radius=10) for i in self.vertices]
+        self.screen.blit(source=self.img_rz, dest=[self.x-self.img_rz.get_width()/2, self.y-self.img_rz.get_height()/2])
 
     @property
     def dest(self) -> [int, int]:
@@ -54,8 +56,7 @@ def mainloop() -> None:
             pygame.quit()
             sys.exit()
     screen.fill(color=(255, 255, 128))
-    huda.draw_huda()
-    # screen.blit(source=mini_img, dest=[100, 100])
+    huda.draw()
     pygame.display.update()
     clock.tick(FRAMES_PER_SECOND)
 
