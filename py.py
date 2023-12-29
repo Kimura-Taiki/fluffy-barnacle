a06e1a74269c60634fd3b803dbde8adfb5a98954
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-12-29 13:52:34 +0900

    J013:uturoもラムダ関数にして定数化

--- py.py
+++ py.py
@@ -8,13 +8,14 @@ from mod.huda import Huda
 
 WX, WY = 1280, 720
 FRAMES_PER_SECOND = 30
-CARDS = 4
+CARDS = 6
 DHX = 120
 DA = -6.0
 HAND_ANGLE = lambda i: -DA/2*(CARDS-1)+DA*i
 HAND_X = lambda i: WX/2-DHX/2*(CARDS-1)+DHX*i
 HAND_Y = lambda i: WY-60+abs(i*2-(CARDS-1))**2*2
 AIHARA_KURO: Callable[[str, int], Surface] = lambda s, i: pygame.font.Font("Aiharahudemojikaisho_free305.ttf", i).render(s, True, (0, 0, 0))
+UTURO: Callable[[int], Surface] = lambda i: pygame.image.load(f"cards/na_00_hajimari_a_n_{i}.png").convert_alpha()
 
 pygame.init()
 pygame.display.set_caption("FFF")
@@ -23,7 +24,8 @@ clock = pygame.time.Clock()
 
 def uturo(i: int) -> pygame.surface.Surface:
     return pygame.image.load(f"cards/na_00_hajimari_a_n_{i}.png").convert_alpha()
-hudas = [Huda(screen=screen, img=uturo(i+1), angle=HAND_ANGLE(i), scale=0.6, x=HAND_X(i), y=HAND_Y(i)) for i in range(CARDS)]
+# hudas = [Huda(screen=screen, img=uturo(i+1), angle=HAND_ANGLE(i), scale=0.6, x=HAND_X(i), y=HAND_Y(i)) for i in range(CARDS)]
+hudas = [Huda(screen=screen, img=UTURO(i+1), angle=HAND_ANGLE(i), scale=0.6, x=HAND_X(i), y=HAND_Y(i)) for i in range(CARDS)]
 
 def mainloop() -> None:
     for event in pygame.event.get():
