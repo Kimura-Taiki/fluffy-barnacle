111d0e7d16059ffba2d9102c62ed98b51d5f2bfe
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-12-30 12:42:11 +0900

    J019:Tehudaクラスで手札の描画位置等を設定

--- mod/tehuda.py
+++ mod/tehuda.py
@@ -1,11 +1,24 @@
+import pygame
+from pygame.surface import Surface
+from pygame import Surface
+from typing import Callable
+
+from mod.const import WX, WY
 from mod.huda import Huda
 from mod.taba import Taba
 
-# taba = Taba(data=[Huda(screen=screen, img=UTURO(i+1), angle=HAND_ANGLE(i), scale=0.6, x=HAND_X(i), y=HAND_Y(i)) for i in range(CARDS)])
+HAND_X_RATE = 120
+HAND_ANGLE_RATE = -6.0
+HAND_ANGLE: Callable[[int, int], int] = lambda i, j: -HAND_ANGLE_RATE/2*(j-1)+HAND_ANGLE_RATE*i
+HAND_X: Callable[[int, int], int] = lambda i, j: WX/2-HAND_X_RATE/2*(j-1)+HAND_X_RATE*i
+HAND_Y: Callable[[int, int], int] = lambda i, j: WY-60+abs(i*2-(j-1))**2*2
 
 class Tehuda(Taba):
     def __init__(self, data: list[Huda]=[]) -> None:
         super().__init__(data)
 
-    # @classmethod
-    # def made_by_files(strs: list[str]) -> "Tehuda":
+    @classmethod
+    def made_by_files(cls, screen: Surface, strs: list[str]) -> "Tehuda":
+        j = len(strs)
+        return Tehuda(data=[Huda(screen=screen, img=v, angle=HAND_ANGLE(i, j), scale=0.6, x=HAND_X(i, j), y=HAND_Y(i, j))
+                            for i, v in enumerate(strs)])
--- py.py
+++ py.py
@@ -5,14 +5,13 @@ import sys
 from mod.const import *
 from mod.huda import Huda
 from mod.taba import Taba
+from mod.tehuda import Tehuda
 
 pygame.init()
 pygame.display.set_caption("FFF")
 screen = pygame.display.set_mode((WX, WY))
 clock = pygame.time.Clock()
-taba = Taba(data=[Huda(screen=screen, img=UTURO(i+1), angle=HAND_ANGLE(i), scale=0.6, x=HAND_X(i), y=HAND_Y(i)) for i in range(CARDS)])
-z = taba[::-1]
-z2 = taba.reverse()
+taba = Tehuda.made_by_files(screen=screen, strs=[UTURO(i) for i in range(1, CARDS+1)])
 
 def mainloop() -> None:
     for event in pygame.event.get():
