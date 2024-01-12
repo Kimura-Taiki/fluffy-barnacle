defb553aa4909bcb3c90c0069c82e764282c5b99
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-01-12 12:46:44 +0900

    J051:MS明朝の32pt専用フォントを作って軽量化

--- mod/const.py
+++ mod/const.py
@@ -14,7 +14,9 @@ clock = pygame.time.Clock()
 BRIGHT = (255, 255, 255, 128)
 
 FRAMES_PER_SECOND = 30
-CARDS = 6
+CARDS = 2
+_MS_MINCHO_32PT_FONT = pygame.font.Font("msmincho001.ttf", 32)
+MS_MINCHO_32PT: Callable[[str], Surface] = lambda s: _MS_MINCHO_32PT_FONT.render(s, True, (0, 0, 0))
 AIHARA_KURO: Callable[[str, int], Surface] = lambda s, i: pygame.font.Font("Aiharahudemojikaisho_free305.ttf", i).render(s, True, (0, 0, 0))
 MS_MINCHO: Callable[[str, int], Surface] = lambda s, i: pygame.font.Font("msmincho001.ttf", i).render(s, True, (0, 0, 0))
 UTURO: Callable[[int], Surface] = lambda i: pygame.image.load(f"cards/na_00_hajimari_a_n_{i}.png").convert_alpha()
--- py.py
+++ py.py
@@ -1,7 +1,7 @@
 import pygame
 from typing import Callable
 
-from mod.const import UTURO, CARDS, screen, clock, FRAMES_PER_SECOND, MS_MINCHO
+from mod.const import UTURO, CARDS, screen, clock, FRAMES_PER_SECOND
 from mod.tehuda import Tehuda
 from mod.controller import controller
 
@@ -10,6 +10,7 @@ controller.get_hover = tehuda.get_hover_huda
 
 def timer_functions() -> tuple[Callable[[], None], Callable[[], None]]:
     from time import time
+    from mod.const import MS_MINCHO_32PT, FRAMES_PER_SECOND
     log = 0.0
     times = [0.01]*FRAMES_PER_SECOND
     def start_timer() -> None:
@@ -20,9 +21,9 @@ def timer_functions() -> tuple[Callable[[], None], Callable[[], None]]:
         elapsed_time = time()-log
         times.append(elapsed_time)
         times.pop(0)
-        screen.blit(source=MS_MINCHO(
-            f"{(sum(times)/FRAMES_PER_SECOND*1000):.2f}ms/Loop, now{round(elapsed_time*1000, 2):.2f}",
-            32), dest=[0, 0])
+        screen.blit(source=MS_MINCHO_32PT(
+            f"{(sum(times)/FRAMES_PER_SECOND*1000):.2f}ms/Loop, now{round(elapsed_time*1000, 2):.2f}"),
+            dest=[0, 0])
     return start_timer, end_timer
 start_timer, end_timer = timer_functions()
 
