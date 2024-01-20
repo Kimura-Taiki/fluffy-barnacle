374739ae042eb2c960d06800036bba09e0b388a2
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-01-20 15:33:09 +0900

    J080:循環参照を防ぐ為にDeliveryプロトコルを創設

--- mod/delivery.py
+++ mod/delivery.py
@@ -1,8 +1,7 @@
 from typing import Protocol
 
+from mod.huda import Huda
+
 class Delivery(Protocol):
     def send_huda_to_ryouiki(self, huda: Huda, is_mine: bool, taba_code: int) -> None:
-        ...
-    
-    def send_ouka_to_ryouiki(self, ryouiki: Ryouiki, is_mine: bool, ryouiki_code: int, count: int) -> None:
         ...
