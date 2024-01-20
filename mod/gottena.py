a1c43975a93e357917475954bbe4a7a7a6969227
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-01-20 09:01:21 +0900

    J078:Youso.set_partial_attrでGottena._hover_gottenonを後付け指定

--- mod/gottena.py
+++ mod/gottena.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 from mod.gottenon import Gottenon
 
 class Gottena(list[Gottenon]):
@@ -5,13 +7,14 @@ class Gottena(list[Gottenon]):
         super().__init__(data)
         self.selected = self[1]
         self.selected.on()
+        [gottenon.set_partial_attr(attr="hover", func=partial(self._hover_gottenon, gottena=self)) for gottenon in self]
 
     def elapse(self) -> None:
         [gottenon.draw() for gottenon in self]
 
     def get_hover_gotten(self) -> Gottenon | None:
         return next((gottenon for gottenon in self[::-1] if gottenon.is_cursor_on()), None)
-    
+
     @staticmethod
     def _hover_gottenon(gottenon: Gottenon, gottena: 'Gottena') -> None:
         if gottena.selected == gottenon:
--- mod/youso.py
+++ mod/youso.py
@@ -1,6 +1,6 @@
 from pygame.surface import Surface
 from pygame.math import Vector2
-from typing import Callable
+from typing import Callable, Any
 from functools import partial
 
 from mod.const import pass_func
@@ -20,6 +20,9 @@ class Youso():
         self.dragstart: Callable[..., None] = partial(kwargs.get('dragstart', pass_func), self)
         self.drag: Callable[..., None] = partial(kwargs.get('drag', pass_func), self)
         self.dragend: Callable[..., None] = partial(kwargs.get('dragend', pass_func), self)
+
+    def set_partial_attr(self, attr: str, func: Callable[[Any], None]) -> None:
+        setattr(self, attr, partial(func, self))
 
     def topleft(self, source: Surface) -> Vector2:
         return self.dest-Vector2(source.get_size())/2
