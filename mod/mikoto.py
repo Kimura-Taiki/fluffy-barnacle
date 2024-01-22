474e1683c97100059ed47407bdeabe7fef22a02b
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-01-23 03:00:40 +0900

    J105:捨て札の描画部分も作成

--- mod/mikoto.py
+++ mod/mikoto.py
@@ -20,7 +20,7 @@ class Mikoto():
         # self.husesute: Taba = husehuda_made_by_files(surfaces=[HONOKA(i) for i in range(2, 4)], delivery=self, is_own=True)
         self.husesute: Taba = husehuda_made_by_files(surfaces=[HONOKA(i) for i in range(2, 4)], delivery=self, is_own=True)
         # self.kirihuda: Taba = tehuda_made_by_files(surfaces=[HONOKA(i) for i in range(4, 7)], delivery=self, is_own=True)
-        self.kirihuda: Taba = sutehuda_made_by_files(surfaces=[HONOKA(i) for i in range(4, 7)], delivery=self, is_own=True)
+        self.kirihuda: Taba = sutehuda_made_by_files(surfaces=[HONOKA(i) for i in range(4, 9)], delivery=self, is_own=True)
         self.gottena: Gottena = Gottena(data=[Gottenon(core_view=self.yamahuda, text="山札", x=WX-140, y=WY-210),
                                               Gottenon(core_view=self.tehuda, text="手札", x=WX-140, y=WY-150),
                                               Gottenon(core_view=self.husesute, text="伏せ札・捨て札", x=WX-140, y=WY-90),
