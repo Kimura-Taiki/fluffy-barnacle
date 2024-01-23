a18850f81303f54828f3542d52a9a9e6826a3009
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-01-24 00:43:48 +0900

    J119:切り札の描画を完成

---
+++ mod/kirihuda.py
@@ -0,0 +1,58 @@
+#                 20                  40                  60                 79
+import pygame
+from pygame.surface import Surface
+from pygame.math import Vector2
+from typing import Callable
+from functools import partial
+
+from mod.const import WX, WY, screen, BRIGHT, ACTION_CIRCLE_NEUTRAL, ACTION_CIRCLE_CARD, ACTION_CIRCLE_BASIC, TC_HUSEHUDA
+from mod.huda import Huda, default_draw
+from mod.taba import Taba
+from mod.controller import controller
+from mod.delivery import Delivery
+
+# HAND_X_RATE: Callable[[int], float] = lambda i: 120-130*max(0, i-3)/i
+HAND_X_RATE: Callable[[int], float] = lambda i: 600/i
+HAND_X: Callable[[int, int], int | float] = lambda i, j: WX/2-HAND_X_RATE(j)/2*(j-1)+HAND_X_RATE(j)*i
+
+# HAND_Y_DIFF: Callable[[int, int], float] = lambda i, j: abs(i*2-(j-1))*(1 if j < 3 else 3/(j-1))
+# HAND_Y: Callable[[int, int], int | float] = lambda i, j: WY-60+HAND_Y_DIFF(i, j)**2*2
+HAND_Y: Callable[[int, int], int | float] = lambda i, j: WY-144
+
+# HAND_ANGLE_RATE: Callable[[int], float] = lambda i: -6 if i < 3 else -6.0*3/(i-1)
+# HAND_ANGLE: Callable[[int, int], int | float] = lambda i, j: -HAND_ANGLE_RATE(j)/2*(j-1)+HAND_ANGLE_RATE(j)*i
+HAND_ANGLE: Callable[[int, int], int | float] = lambda i, j: 0
+
+def kirihuda_made_by_files(surfaces: list[Surface], delivery: Delivery, is_own: bool) -> Taba:
+    tehuda = Taba(delivery=delivery, is_own=is_own, inject=_inject_of_tehuda)
+    tehuda.var_rearrange = partial(_rearrange_tehuda, taba=tehuda)
+    for i in surfaces:
+        tehuda.append(Huda(img=i))
+    return tehuda
+
+def _rearrange_tehuda(taba: Taba) -> None:
+    angle_func, x_func, y_func = _rearrange_funcs(l=len(taba), is_own=taba.is_own)
+    [huda.rearrange(angle=angle_func(i), scale=0.6, x=x_func(i), y=y_func(i)) for i, huda in enumerate(taba)]
+
+def _rearrange_funcs(l: int, is_own: bool) -> tuple[Callable[[int], float], Callable[[int], float], Callable[[int], float]]:
+    if is_own:
+        return partial(HAND_ANGLE, j=l), partial(HAND_X, j=l), partial(HAND_Y, j=l)
+    else:
+        return (partial(lambda i, j: HAND_ANGLE(i, j)+180.0, j=l),
+                partial(lambda i, j: WX-HAND_X(i, j), j=l), partial(lambda i, j: WY-HAND_Y(i, j), j=l))
+
+def _inject_of_tehuda(huda: Huda, taba: Taba) -> None:
+    huda.inject_funcs(draw=_draw, hover=_hover)
+    
+def _draw(huda: Huda) -> None:
+    if controller.active == huda:
+        return None
+    elif controller.hover == huda:
+        pygame.draw.polygon(screen, BRIGHT, [i+[0, -40] for i in huda.vertices], 20)
+        screen.blit(source=huda.img_rz, dest=huda.img_rz_topleft+[0, -40])
+    else:
+        default_draw(huda=huda)
+    return None
+
+def _hover(huda: Huda) -> None:
+    screen.blit(source=huda.img_nega, dest=[WX-huda.img_nega.get_width(), 0])
--- mod/mikoto.py
+++ mod/mikoto.py
@@ -4,13 +4,14 @@ from functools import partial
 from mod.const import UTURO, HONOKA, CARDS, WX, WY, TC_YAMAHUDA, TC_TEHUDA\
     , TC_SUTEHUDA, TC_HUSEHUDA, TC_KIRIHUDA, TC_MISIYOU, TC_ZYOGAI, screen\
     , IMG_AURA_AREA, IMG_FLAIR_AREA, IMG_LIFE_AREA, IMG_SYUUTYUU_AREA\
-    , compatible_with
+    , compatible_with, HONOKA_S
 from mod.gottenon import Gottenon
 from mod.gottena import Gottena
 from mod.yamahuda import yamahuda_made_by_files
 from mod.tehuda import tehuda_made_by_files
 from mod.husehuda import husehuda_made_by_files
 from mod.sutehuda import sutehuda_made_by_files
+from mod.kirihuda import kirihuda_made_by_files
 from mod.huse_sute_view import HuseSuteView
 from mod.huda import Huda
 from mod.taba import Taba
@@ -23,7 +24,7 @@ class Mikoto():
         self.tehuda: Taba = tehuda_made_by_files(surfaces=[UTURO(i) for i in range(1, CARDS+1)], delivery=self, is_own=True)
         self.husehuda: Taba = husehuda_made_by_files(surfaces=[HONOKA(i) for i in range(2, 4)], delivery=self, is_own=True)
         self.sutehuda: Taba = sutehuda_made_by_files(surfaces=[HONOKA(i) for i in range(4, 9)], delivery=self, is_own=True)
-        self.kirihuda: Taba = tehuda_made_by_files(surfaces=[HONOKA(i) for i in range(4, 7)], delivery=self, is_own=True)
+        self.kirihuda: Taba = kirihuda_made_by_files(surfaces=[HONOKA_S(i) for i in range(1, 4)], delivery=self, is_own=True)
         self.gottena: Gottena = Gottena(data=[Gottenon(core_view=self.yamahuda, name="山札", x=WX-140, y=WY-210),
                                               Gottenon(core_view=self.tehuda, name="手札", x=WX-140, y=WY-150),
                                               Gottenon(core_view=HuseSuteView(husehuda=self.husehuda, sutehuda=self.sutehuda),
