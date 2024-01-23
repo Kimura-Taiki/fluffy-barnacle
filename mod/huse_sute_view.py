4cd74a590bb558c7d1755a9c3ab8a4e1912414a2
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-01-23 14:26:15 +0900

    J108:HuseSuteViewの内部だけ作成

--- mod/huse_sute_view.py
+++ mod/huse_sute_view.py
@@ -1,22 +1,25 @@
 from mod.const import compatible_with
 from mod.core_view import CoreView
 from mod.huda import Huda
+from mod.taba import Taba
 
 class HuseSuteView():
-    def __init__(self) -> None:
-        pass
-        # super().__init__()
+    def __init__(self, husehuda: Taba, sutehuda: Taba) -> None:
+        self.husehuda = husehuda
+        self.sutehuda = sutehuda
 
     def elapse(self) -> None:
-        pass
-        # return super().elapse()
+        self.husehuda.elapse()
+        self.sutehuda.elapse()
     
     def rearrange(self) -> None:
-        pass
-        # return super().rearrange()
+        self.husehuda.rearrange()
+        self.sutehuda.rearrange()
     
     def get_hover_huda(self) -> Huda | None:
-        pass
-        # return super().get_hover_huda()
+        if huda := self.sutehuda.get_hover_huda():
+            return huda
+        else:
+            return self.husehuda.get_hover_huda()
 
 compatible_with(cls=HuseSuteView, protocol=CoreView)
