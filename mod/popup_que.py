10ceb2e61eba1f00d1211ecd43f1a9be16e77c12
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-01-27 04:36:11 +0900

    J153:PopupMessage本体も書き上げる

--- mod/popup_que.py
+++ mod/popup_que.py
@@ -3,21 +3,35 @@ from pygame.math import Vector2
 
 from mod.const import screen, WX, WY, BLACK, WHITE, MS_MINCHO_32PT
 
+class _Queue:
+    def __init__(self, text: str, color: tuple[int, int, int] = BLACK) -> None:
+        self.img_text = MS_MINCHO_32PT(text)
+        self.img_shadow = Surface(self.img_text.get_size())
+        self.img_shadow.fill(WHITE)
+        self.lifetime: int = 60
+        self.coord = Vector2(WX/2, WY-240)-Vector2(self.img_text.get_size())/2
+
+    def draw(self) -> None:
+        self.img_shadow.set_alpha(128)
+        screen.blit(source=self.img_shadow, dest=self.coord)
+        self.img_text.set_alpha(255)
+        screen.blit(source=self.img_text, dest=self.coord)
+
 class PopupMessage:
-    class Queue:
-        def __init__(self, text: str, color: tuple[int, int, int] = BLACK) -> None:
-            self.img_text = MS_MINCHO_32PT(text)
-            self.img_shadow = Surface(self.img_text.get_size())
-            self.img_shadow.fill(WHITE)
-            self.lifetime: int = 60
-            self.coord = Vector2(WX/2, WY-240)-Vector2(self.img_text.get_size())/2
-
-        def draw(self) -> None:
-            self.img_shadow.set_alpha(128)
-            screen.blit(source=self.img_shadow, dest=self.coord)
-            self.img_text.set_alpha(255)
-            screen.blit(source=self.img_text, dest=self.coord)
-
     def __init__(self) -> None:
-        pass
-        # self.queues = 
+        self.queues: list[_Queue] = []
+
+    def add(self, text: str) -> None:
+        self.queues.append(_Queue(text=text))
+
+    def draw(self) -> None:
+        for queue in self.queues:
+            queue.draw()
+
+    def elapse(self) -> None:
+        for queue in self.queues[::-1]:
+            queue.lifetime -= 1
+            if queue.lifetime <= 0:
+                self.queues.remove(queue)
+
+popup_message = PopupMessage()
