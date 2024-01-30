f316e3ff7f2ded8a180f618848769d95c786dffe
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-01-30 14:25:19 +0900

    J181:partialを省いてcard.pyのマクロを単純化

--- mod/card.py
+++ mod/card.py
@@ -10,10 +10,10 @@ SuuziDI = Callable[[Delivery, int], int]
 MaaiDI = Callable[[Delivery, int], list[bool]]
 auto_di: BoolDI = lambda delivery, hoyuusya: True
 pass_di: KoukaDI = lambda delivery, hoyuusya: None
-int_di: Callable[[int], SuuziDI] = lambda i: partial(lambda delivery, hoyuusya, teisuu: teisuu, teisuu=i)
+int_di: Callable[[int], SuuziDI] = lambda i: lambda delivery, hoyuusya: i
 whole_di: MaaiDI = lambda delivery, hoyuusya: [True]*11
-moma_di: Callable[[int], MaaiDI] = lambda i: partial(lambda delivery, hoyuusya, teisuu: [j == teisuu for j in range(11)], teisuu=i)
-dima_di: Callable[[int, int], MaaiDI] = lambda i, j: partial(lambda delivery, hoyuusya, minsuu, maxsuu: [minsuu <= k and k <= maxsuu for k in range(11)], minsuu=i, maxsuu=j)
+moma_di: Callable[[int], MaaiDI] = lambda i: lambda delivery, hoyuusya: [j == i for j in range(11)]
+dima_di: Callable[[int, int], MaaiDI] = lambda i, j: lambda delivery, hoyuusya: [i <= k <= j for k in range(11)]
 
 class Card():
     def __init__(self, img: Surface, cond: BoolDI) -> None:
@@ -21,5 +21,8 @@ class Card():
         self.cond = cond
 
 class Kougeki(Card):
-    def __init__(self, img: Surface, cond: BoolDI, aura_damage: SuuziDI, life_dagage: SuuziDI) -> None:
+    def __init__(self, img: Surface, cond: BoolDI, life_dagage: SuuziDI, aura_damage: SuuziDI, maai_list: MaaiDI) -> None:
         super().__init__(img, cond)
+        self.life_dagage = life_dagage
+        self.aura_damage =aura_damage
+        self.maai_list = maai_list
