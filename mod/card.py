49246115f3a191ce2ffca0f9639ff3f75daf1398
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-02-01 17:55:27 +0900

    J202:ダメージ解決をDamage.kaiketu命令に転換

--- mod/card.py
+++ mod/card.py
@@ -25,6 +25,9 @@ class Card():
         self.cond = cond
         self.type = CT_HUTEI
 
+    def kaiketu(self, delivery: Delivery, hoyuusya: int) -> None:
+        pass
+
 class Kougeki(Card):
     def __init__(self, img: Surface, name: str, cond: BoolDI,
                  aura_damage: SuuziDI, life_damage: SuuziDI, maai_list: MaaiDI) -> None:
@@ -59,11 +62,14 @@ class Damage(Card):
 
     def __init__(self, img: Surface, name: str, dmg: int, from_code: int, to_code: int) -> None:
         super().__init__(img, name, auto_di)
+        self.img = img.copy()
         self.dmg = dmg
         self.from_code = from_code
         self.to_code = to_code
         draw_aiharasuu(surface=self.img, dest=Vector2(340, 475)/2 - Vector2(self._SCALE_SIZE, self._SCALE_SIZE)/2,
                        num=dmg, size=self._SCALE_SIZE)
 
-    def damage(self, delivery: Delivery, hoyuusya: int) -> None:
-        delivery.send_ouka_to_ryouiki(hoyuusya=int, from_mine=False, from_code=self.from_code, to_mine=False, to_code=self.to_code)
+    def kaiketu(self, delivery: Delivery, hoyuusya: int) -> None:
+        delivery.send_ouka_to_ryouiki(hoyuusya=hoyuusya, from_mine=False, from_code=self.from_code,
+                                      to_mine=False, to_code=self.to_code, kazu=self.dmg)
+
--- mod/ol/play_kougeki.py
+++ mod/ol/play_kougeki.py
@@ -9,7 +9,7 @@ from mod.const import screen, IMG_GRAY_LAYER, compatible_with, IMG_AURA_DAMAGE, 
 from mod.ol.over_layer import OverLayer
 from mod.huda import Huda, default_draw
 from mod.ol.view_banmen import view_youso
-from mod.card import Card, auto_di, Kougeki, SuuziDI
+from mod.card import Card, auto_di, Kougeki, SuuziDI, Damage
 from mod.taba import Taba
 from mod.tf.taba_factory import TabaFactory
 from mod.controller import controller
@@ -20,9 +20,6 @@ HAND_X: Callable[[int, int], float] = lambda i, j: WX/2-110*(j-1)+220*i
 HAND_Y: Callable[[int, int], float] = lambda i, j: WY/2-150
 HAND_ANGLE: Callable[[int, int], float] = lambda i, j: 0.0
 SCALE_SIZE = 180
-
-_ad_card = Card(img=IMG_AURA_DAMAGE, name="", cond=auto_di)
-_ld_card = Card(img=IMG_LIFE_DAMAGE, name="", cond=auto_di)
 
 class PlayKougeki():
     def __init__(self, huda: Huda) -> None:
@@ -40,9 +37,6 @@ class PlayKougeki():
         screen.blit(source=self.kougeki.img, dest=-Vector2(self.kougeki.img.get_size())/2+Vector2(WX, WY)/2)
         screen.blit(source=IMG_GRAY_LAYER, dest=[0, 0])
         self.uke_taba.elapse()
-        self._draw_damage(_ad_card, self.kougeki.aura_damage)
-        self._draw_damage(_ld_card, self.kougeki.life_damage)
-
 
     def get_hover(self) -> Any | None:
         return self.uke_taba.get_hover_huda() or view_youso
@@ -51,6 +45,10 @@ class PlayKougeki():
         bac = TabaFactory(inject_kwargs={
             "draw": self._draw, "hover": Huda.detail_draw, "mousedown": self._mousedown, "mouseup": self._mouseup
             }, huda_x=HAND_X, huda_y=HAND_Y, huda_angle=HAND_ANGLE)
+        _ad_card = Damage(img=IMG_AURA_DAMAGE, name="オーラダメージ", dmg=self.kougeki.aura_damage(
+            self.delivery, self.source_huda.hoyuusya), from_code=UC_AURA, to_code=UC_DUST)
+        _ld_card = Damage(img=IMG_LIFE_DAMAGE, name="ライフダメージ", dmg=self.kougeki.life_damage(
+            self.delivery, self.source_huda.hoyuusya), from_code=UC_LIFE, to_code=UC_FLAIR)
         self.uke_taba = bac.maid_by_cards(cards=[_ad_card, _ld_card], hoyuusya=self.source_huda.hoyuusya)
 
     def close(self) -> int:
@@ -58,12 +56,6 @@ class PlayKougeki():
 
     def moderate(self, stat: int) -> None:
         ...
-
-    def _draw_damage(self, card_type: Card, damage_func: SuuziDI) -> None:
-        if huda := next((huda for huda in self.uke_taba if huda.card == card_type), None):
-            draw_aiharasuu(surface=screen, dest=huda.dest - Vector2(SCALE_SIZE, SCALE_SIZE)/2,
-                           num=damage_func(self.delivery, self.source_huda.hoyuusya),
-                           size=SCALE_SIZE)
 
     def _draw(self, huda: Huda) -> None:
         if controller.hover == huda:
@@ -77,25 +69,9 @@ class PlayKougeki():
 
     def _mouseup(self, huda: Huda) -> None:
         popup_message.add(text="PlayKougeki.mouseup でクリック確定したよ")
-        
-        if (do := {_ad_card: self._receive_aura_damage, _ld_card: self._receive_life_damage}.get(huda.card)) is None:
-            raise ValueError(f"Invalid huda.card: {huda.card}")
-        do()
-        huda.koudou(self.delivery, self.delivery.turn_player)
+        huda.card.kaiketu(delivery=self.delivery, hoyuusya=self.source_huda.hoyuusya)
         self.delivery.send_huda_to_ryouiki(huda=self.source_huda, is_mine=True, taba_code=TC_SUTEHUDA)
         moderator.pop()
-
-    def _receive_aura_damage(self) -> None:
-        popup_message.add(text="オーラで受けました")
-        self.delivery.send_ouka_to_ryouiki(
-            hoyuusya=self.source_huda.hoyuusya, from_mine=False, from_code=UC_AURA, to_mine=False, to_code=UC_DUST,
-            kazu=self.kougeki.aura_damage(self.delivery, self.source_huda.hoyuusya))
-
-    def _receive_life_damage(self) -> None:
-        popup_message.add(text="ライフで受けました")
-        self.delivery.send_ouka_to_ryouiki(
-            hoyuusya=self.source_huda.hoyuusya, from_mine=False, from_code=UC_LIFE, to_mine=False, to_code=UC_FLAIR,
-            kazu=self.kougeki.life_damage(self.delivery, self.source_huda.hoyuusya))
 
 
 # compatible_with(, OverLayer)
