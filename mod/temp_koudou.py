ee1bc85eee5d58aa7103bdcea210a1733c46011b
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-02-03 20:46:18 +0900

    J213:処理が長くなったのでTempCardをTempKoudouに改名して分離

---
+++ mod/temp_koudou.py
@@ -0,0 +1,48 @@
+from pygame.surface import Surface
+
+from mod.const import IMG_HAKUSI, MS_MINCHO_COL, BLACK, IMG_FT_ARROW, IMG_FT_OUKA, FONT_SIZE_CARD_TITLE\
+    , IMG_FT_MAAI, IMG_FT_DUST, IMG_FT_ZYOGAI, IMG_FT_AI_AURA, IMG_FT_AI_FLAIR, IMG_FT_AI_LIFE, IMG_FT_AI_SYUUTYUU\
+    , IMG_FT_ZI_AURA, IMG_FT_ZI_FLAIR, IMG_FT_ZI_LIFE, IMG_FT_ZI_SYUUTYUU, UC_MAAI, UC_DUST, UC_ZYOGAI\
+    , UC_AURA, UC_FLAIR, UC_LIFE, UC_SYUUTYUU
+from mod.card import Koudou, BoolDI, KoukaDI
+
+class TempKoudou(Koudou):
+    def __init__(self, name: str, cond: BoolDI, kouka: KoukaDI, todo: list[list]="") -> None:
+        self.img = IMG_HAKUSI.copy()
+        self.y = 20
+        self._draw_name(name=name)
+        for i in todo:
+            if isinstance(i[0], bool):
+                self._draw_yazirusi(from_mine=i[0], from_code=i[1], to_mine=i[2], to_code=i[3], kazu=i[4])
+            elif isinstance(i[0], str):
+                self._draw_text(texts=i)
+        super().__init__(self.img, name, cond, kouka)
+
+    def _draw_name(self, name: str) -> None:
+        for i, mozi in enumerate(name):
+            self.img.blit(source=MS_MINCHO_COL(mozi, FONT_SIZE_CARD_TITLE, BLACK), dest=[0, i*FONT_SIZE_CARD_TITLE])
+
+    def _draw_yazirusi(self, from_mine: bool, from_code: int, to_mine: bool, to_code: int, kazu: int) -> None:
+        self.img.blit(source=self._img_target(is_mine=from_mine, utuwa_code=from_code), dest=[60, self.y])
+        self.img.blit(source=self._img_target(is_mine=to_mine, utuwa_code=to_code), dest=[210, self.y])
+        self.img.blit(source=IMG_FT_ARROW, dest=[130, self.y])
+        for i in range(kazu):
+            self.img.blit(source=IMG_FT_OUKA, dest=[140-kazu*5+i*10, self.y])
+        self.y += 100
+
+    def _img_target(self, is_mine: bool, utuwa_code: int) -> Surface:
+        if not (target := (
+            {UC_MAAI: IMG_FT_MAAI, UC_DUST: IMG_FT_DUST, UC_ZYOGAI: IMG_FT_ZYOGAI} |
+            ({UC_AURA: IMG_FT_ZI_AURA, UC_FLAIR: IMG_FT_ZI_FLAIR, UC_LIFE: IMG_FT_ZI_LIFE, UC_SYUUTYUU: IMG_FT_ZI_SYUUTYUU}
+             if is_mine else
+             {UC_AURA: IMG_FT_AI_AURA, UC_FLAIR: IMG_FT_AI_FLAIR, UC_LIFE: IMG_FT_AI_LIFE,UC_SYUUTYUU: IMG_FT_AI_SYUUTYUU}))
+             .get(utuwa_code)):
+            raise ValueError(f"Invalid utuwa_code: {utuwa_code}")
+        return target
+    
+    def _draw_text(self, texts: list[str]) -> None:
+        self.y += 5
+        for text in texts:
+            self.img.blit(source=MS_MINCHO_COL(text, 20, BLACK), dest=[60, self.y])
+            self.y += 20+2
+        self.y += 5
--- z.py
+++ z.py
@@ -14,9 +14,10 @@ import sys
 
 # 画像の読み込み
 # life = pygame.image.load("pictures/area_life.png")
-from mod.card import TempCard, auto_di
+from mod.card import auto_di, pass_di
+from mod.temp_koudou import TempKoudou
 from mod.const import UC_MAAI, UC_DUST, UC_AURA, UC_FLAIR
-tc = TempCard(name="潜り", cond=auto_di, todo=[
+tc = TempKoudou(name="潜り", cond=auto_di, kouka=pass_di, todo=[
     ["コストとして、あなたの", "マシンにある造花結晶を", "２つ燃焼済みにして良い。", "そうした場合、あなたは", "集中力を１得て、相手は", "集中力１を失い、相手を", "萎縮させる。"], [False, UC_MAAI, False, UC_DUST, 3], [True, UC_AURA, False, UC_FLAIR, 1],
     ["ほいほいほいほいほい", ]])
 from mod.const import IMG_HAKUSI
