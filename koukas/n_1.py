3bc7751d441f6cb7f352a59b7157aa2152fc464e
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-02-04 11:38:42 +0900

    J214:Choiceを書き上げる

--- koukas/n_1.py
+++ koukas/n_1.py
@@ -1,9 +1,11 @@
 import pygame
 
-from mod.const import UC_ZYOGAI, UC_SYUUTYUU
+from mod.const import UC_ZYOGAI, UC_SYUUTYUU, UC_MAAI, UC_DUST
 from mod.card import Kougeki, Koudou, auto_di, int_di, dima_di, KoukaDI
+from mod.temp_koudou import TempKoudou
 from mod.delivery import Delivery
 from mod.moderator import moderator
+from mod.ol.choice import Choice
 
 n_1 = Kougeki(img=pygame.image.load("cards/na_00_hajimari_a_n_1.png"), name="投射", cond=auto_di,
               aura_damage=int_di(3), life_damage=int_di(1), maai_list=dima_di(5, 9))
@@ -17,9 +19,18 @@ n_3 = Kougeki(img=pygame.image.load("cards/na_00_hajimari_a_n_3.png"), name="牽
 n_4 = Kougeki(img=pygame.image.load("cards/na_00_hajimari_a_n_4.png"), name="背中刺し", cond=auto_di,
               aura_damage=int_di(3), life_damage=int_di(2), maai_list=dima_di(1, 1))
 
+def _kouka_n_6_1(delivery: Delivery, hoyuusya: int) -> None:
+    delivery.send_ouka_to_ryouiki(hoyuusya=hoyuusya, from_mine=False, from_code=UC_MAAI, to_mine=False, to_code=UC_DUST, kazu=1)
+
+def _kouka_n_6_2(delivery: Delivery, hoyuusya: int) -> None:
+    delivery.send_ouka_to_ryouiki(hoyuusya=hoyuusya, from_mine=False, from_code=UC_DUST, to_mine=False, to_code=UC_MAAI, kazu=1)
+
+tkn61 = TempKoudou(name="潜り", cond=auto_di, kouka=_kouka_n_6_1, todo=[[False, UC_MAAI, False, UC_DUST, 1]])
+tkn62 = TempKoudou(name="離脱", cond=auto_di, kouka=_kouka_n_6_2, todo=[[False, UC_DUST, False, UC_MAAI, 1]])
+
 def _kouka_n_6(delivery: Delivery, hoyuusya: int) -> None:
-    delivery.send_ouka_to_ryouiki(hoyuusya=hoyuusya, from_mine=False, from_code=UC_ZYOGAI, to_mine=True, to_code=UC_SYUUTYUU)
-    # delivery.send_ouka_to_ryouiki(hoyuusya=hoyuusya, from_mine=False, from_code=UC_MAAI, )
+    delivery.send_ouka_to_ryouiki(hoyuusya=hoyuusya, from_mine=False, from_code=UC_ZYOGAI, to_mine=True, to_code=UC_SYUUTYUU, kazu=1)
+    moderator.append(over_layer=Choice(cards=[tkn61, tkn62], delivery=delivery, hoyuusya=hoyuusya))
 
 n_6 = Koudou(img=pygame.image.load("cards/na_00_hajimari_a_n_6.png"), name="歩法", cond=auto_di, kouka=_kouka_n_6)
 
--- mod/ol/choice.py
+++ mod/ol/choice.py
@@ -23,34 +23,26 @@ HAND_ANGLE: Callable[[int, int], float] = lambda i, j: 0.0
 SCALE_SIZE = 180
 
 class Choice():
-    def __init__(self, kougeki: Kougeki, delivery: Delivery, hoyuusya: int, huda: Any | None) -> None:
-        self.kougeki = kougeki
+    def __init__(self, cards: list[Card], delivery: Delivery, hoyuusya: int, huda: Any | None=None) -> None:
         self.delivery = delivery
         self.hoyuusya = hoyuusya
         self.source_huda = huda if isinstance(huda, Huda) else None
-        self.name = f"攻撃:{kougeki.name}の使用"
+        self.name = "効果の選択"
         self.inject_func = delivery.inject_view
-        self.taiou_taba: Taba = Taba()
-        self.uke_taba: Taba = Taba()
-
-    def elapse(self) -> None:
-        screen.blit(source=self.kougeki.img, dest=-Vector2(self.kougeki.img.get_size())/2+Vector2(WX, WY)/2)
-        screen.blit(source=IMG_GRAY_LAYER, dest=[0, 0])
-        self.uke_taba.elapse()
-
-    def get_hover(self) -> Any | None:
-        return self.uke_taba.get_hover_huda() or view_youso
-
-    def open(self) -> None:
-        bac = TabaFactory(inject_kwargs={
+        factory = TabaFactory(inject_kwargs={
             "draw": available_draw, "hover": Huda.detail_draw, "mousedown": self._mousedown, "mouseup": self._mouseup
             }, huda_x=HAND_X, huda_y=HAND_Y, huda_angle=HAND_ANGLE)
-        _ad_card = Damage(img=IMG_AURA_DAMAGE, name="オーラダメージ", dmg=self.kougeki.aura_damage(
-            self.delivery, self.hoyuusya), from_code=UC_AURA, to_code=UC_DUST)
-        can_receive_aura = _ad_card.can_damage(delivery=self.delivery, hoyuusya=self.hoyuusya)
-        _ld_card = Damage(img=IMG_LIFE_DAMAGE, name="ライフダメージ", dmg=self.kougeki.life_damage(
-            self.delivery, self.hoyuusya), from_code=UC_LIFE, to_code=UC_FLAIR)
-        self.uke_taba = bac.maid_by_cards(cards=([_ad_card, _ld_card] if can_receive_aura else [_ld_card]), hoyuusya=self.hoyuusya)
+        self.taba = factory.maid_by_cards(cards=cards, hoyuusya=self.hoyuusya)
+
+    def elapse(self) -> None:
+        screen.blit(source=IMG_GRAY_LAYER, dest=[0, 0])
+        self.taba.elapse()
+
+    def get_hover(self) -> Any | None:
+        return self.taba.get_hover_huda() or view_youso
+
+    def open(self) -> None:
+        ...
 
     def close(self) -> int:
         return 0
