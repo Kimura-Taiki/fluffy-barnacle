285be1012ce9205c0a912b50f7fa94fec6551ab6
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-02-10 03:21:47 +0900

    J252:Buttonクラスを創設、軽く実装

--- mod/ol/main_phase.py
+++ mod/ol/main_phase.py
@@ -1,22 +1,42 @@
+import pygame
+from pygame.surface import Surface
+from pygame.math import Vector2
 from typing import Callable
 
-from mod.const import compatible_with, pass_func
+from mod.const import compatible_with, pass_func, screen, IMG_ZYOGAI_AREA, WX, WY
 from mod.ol.over_layer import OverLayer
 from mod.youso import Youso
 from mod.popup_message import popup_message
 from mod.delivery import Delivery, duck_delivery
+
+class Button(Youso):
+    def __init__(self, img: Surface, x: int | float = 0, y: int | float = 0) -> None:
+        super().__init__(x, y, draw=Button._draw, mousedown=Button._mousedown)
+        self.img = img
+
+    def is_cursor_on(self) -> bool:
+        mx, my = pygame.mouse.get_pos()
+        [hx, hy] = Vector2(self.img.get_size())/2
+        return self.x-hx <= mx and mx <= self.x+hx and self.y-hy <= my and my <= self.y+hy
+
+    def _draw(self) -> None:
+        screen.blit(source=self.img, dest=self.dest-Vector2(self.img.get_size())/2)
+
+    def _mousedown(self) -> None:
+        popup_message.add(text="ボタンを押したよ")
 
 class MainPhase():
     def __init__(self, inject_func: Callable[[], None]=pass_func, delivery: Delivery=duck_delivery) -> None:
         self.name = "メインフェイズ"
         self.inject_func: Callable[[], None] = inject_func
         self.delivery = delivery
+        self.button = Button(img=IMG_ZYOGAI_AREA, x=WX/2, y=WY/2)
 
     def elapse(self) -> None:
-        ...
+        self.button.draw()
 
     def get_hover(self) -> Youso | None:
-        return None
+        return self.button if self.button.is_cursor_on() else None
 
     def open(self) -> None:
         ...
