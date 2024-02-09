e18e94a223edb32800f4a67a9d6187eecd929a48
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-02-09 10:15:36 +0900

    J250:対応時に切り札も対応候補に投入

--- mod/ol/taiou_taba.py
+++ mod/ol/taiou_taba.py
@@ -1,6 +1,6 @@
 from typing import Callable
 
-from mod.const import WX, WY, TC_TEHUDA
+from mod.const import WX, WY, TC_TEHUDA, TC_KIRIHUDA
 from mod.huda import Huda
 from mod.taba import Taba
 from mod.tf.taba_factory import TabaFactory
@@ -19,8 +19,10 @@ def make_taiou_taba(delivery: Delivery, hoyuusya: int) -> Taba:
     global taiou_taba, origin_list
     if not isinstance(tehuda := delivery.taba_target(hoyuusya=hoyuusya, is_mine=False, taba_code=TC_TEHUDA), Taba):
         raise ValueError(f"Invalid tehuda: {tehuda}")
+    if not isinstance(kirihuda := delivery.taba_target(hoyuusya=hoyuusya, is_mine=False, taba_code=TC_KIRIHUDA), Taba):
+        raise ValueError(f"Invalid kirihuda: {kirihuda}")
     factory = _taiou_factory(mouseup=_taiou_mouseup)
-    origin_list = [huda for huda in tehuda if huda.card.taiou]
+    origin_list = [huda for huda in tehuda+kirihuda if huda.card.taiou]
     taiou_taba = factory.maid_by_cards(cards=[huda.card for huda in origin_list], hoyuusya=hoyuusya)
     return taiou_taba
 
