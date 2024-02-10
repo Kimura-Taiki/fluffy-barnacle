3a84f7f6d867639c8cb1e97c19b39f5737611a9c
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-02-10 22:34:21 +0900

    J256:Cardの統合前にCardの引数を整理

--- mod/card.py
+++ mod/card.py
@@ -1,9 +1,11 @@
+#                 20                  40                  60                 79
 from pygame.surface import Surface
 from pygame.math import Vector2
 from typing import Callable, Any
 from functools import partial
 
-from mod.const import CT_HUTEI, CT_KOUGEKI, draw_aiharasuu, UC_MAAI, TC_SUTEHUDA, SIMOTE, KAMITE, side_name, UC_FLAIR, UC_DUST
+from mod.const import CT_HUTEI, CT_KOUGEKI, draw_aiharasuu, UC_MAAI, TC_SUTEHUDA, SIMOTE, KAMITE, side_name, UC_FLAIR\
+    , CT_KOUDOU, UC_DUST
 from mod.delivery import Delivery
 from mod.popup_message import popup_message
 from mod.moderator import moderator
@@ -21,13 +23,14 @@ moma_di: Callable[[int], MaaiDI] = lambda i: lambda delivery, hoyuusya: [j == i 
 dima_di: Callable[[int, int], MaaiDI] = lambda i, j: lambda delivery, hoyuusya: [i <= k <= j for k in range(11)]
 identity_di: TaiounizeDI = lambda i, j, k: i
 
+#                 20                  40                  60                 79
 class Card():
-    def __init__(self, img: Surface, name: str, cond: BoolDI, taiou: bool=False, zenryoku: bool=False, kirihuda: bool=False,
+    def __init__(self, img: Surface, name: str, cond: BoolDI, type: int=CT_HUTEI, kouka: KoukaDI=pass_di,
+                 taiou: bool=False, zenryoku: bool=False, kirihuda: bool=False,
                  flair: SuuziDI=int_di(0), taiounize: TaiounizeDI = identity_di) -> None:
-        self.img = img
-        self.name = name
-        self.cond = cond
-        self.type = CT_HUTEI
+        self.img, self.name, self.cond = img, name, cond
+        self.type = type
+        self.kouka =kouka
         self.taiou = taiou
         self.flair = flair
         self.zenryoku = zenryoku
@@ -54,11 +57,27 @@ class Card():
     def close(self, hoyuusya: int) -> None:
         popup_message.add(f"{side_name(hoyuusya)}の「{self.name}」を解決しました")
 
+class Koudou(Card):
+    def __init__(self, img: Surface, name: str, cond: BoolDI, kouka: KoukaDI, taiou: bool = False, zenryoku: bool = False,
+                 kirihuda: bool = False, flair: SuuziDI = int_di(0), taiounize: TaiounizeDI = identity_di) -> None:
+        super().__init__(img, name, cond, CT_KOUDOU, kouka, taiou, zenryoku, kirihuda, flair, taiounize)
+        # self.kouka = kouka
+
+    def kaiketu(self, delivery: Delivery, hoyuusya: int, huda: Any | None = None) -> None:
+        super().kaiketu(delivery, hoyuusya, huda)
+        self.kouka(delivery, hoyuusya)
+        from mod.huda import Huda
+        if isinstance(huda, Huda):
+            huda.discard()
+        self.close(hoyuusya=hoyuusya)
+
+
+
 class Kougeki(Card):
     def __init__(self, img: Surface, name: str, cond: BoolDI, aura_damage: SuuziDI, life_damage: SuuziDI, maai_list: MaaiDI,
                  taiou: bool = False, zenryoku: bool = False, kirihuda: bool = False, flair: SuuziDI = int_di(0),
                  taiounize: TaiounizeDI = identity_di) -> None:
-        super().__init__(img, name, cond, taiou, zenryoku, kirihuda, flair, taiounize)
+        super().__init__(img, name, cond, CT_KOUGEKI, pass_di, taiou, zenryoku, kirihuda, flair, taiounize)
         self.type = CT_KOUGEKI
         self.aura_damage = aura_damage
         self.life_damage = life_damage
@@ -99,20 +118,6 @@ class Kougeki(Card):
 
     def maai_cond(self, delivery: Delivery, hoyuusya: int) -> bool:
         return self.maai_list(delivery, hoyuusya)[delivery.ouka_count(hoyuusya=hoyuusya, is_mine=True, utuwa_code=UC_MAAI)]
-
-class Koudou(Card):
-    def __init__(self, img: Surface, name: str, cond: BoolDI, kouka: KoukaDI, taiou: bool = False, zenryoku: bool = False,
-                 kirihuda: bool = False, flair: SuuziDI = int_di(0), taiounize: TaiounizeDI = identity_di) -> None:
-        super().__init__(img, name, cond, taiou, zenryoku, kirihuda, flair, taiounize)
-        self.kouka = kouka
-
-    def kaiketu(self, delivery: Delivery, hoyuusya: int, huda: Any | None = None) -> None:
-        super().kaiketu(delivery, hoyuusya, huda)
-        self.kouka(delivery, hoyuusya)
-        from mod.huda import Huda
-        if isinstance(huda, Huda):
-            huda.discard()
-        self.close(hoyuusya=hoyuusya)
 
 class Damage(Card):
     _SCALE_SIZE = 180
