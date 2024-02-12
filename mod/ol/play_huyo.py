2c1a6e62c69c996a920c7c3c98c8ff049531b029
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-02-12 18:09:24 +0900

    J281:数字や諸関数を設定

--- mod/ol/play_huyo.py
+++ mod/ol/play_huyo.py
@@ -5,7 +5,8 @@ from pygame.transform import scale
 from typing import Any
 
 from mod.const import screen, IMG_GRAY_LAYER, compatible_with, WX, WY, IMG_DECISION, IMG_DECISION_LIGHTEN,\
-    IMG_OSAME_DUST, IMG_OSAME_DUST_LIGHTEN, IMG_OSAME_AURA, IMG_OSAME_AURA_LIGHTEN, draw_aiharasuu, FONT_SIZE_OSAME_NUM
+    IMG_OSAME_DUST, IMG_OSAME_DUST_LIGHTEN, IMG_OSAME_AURA, IMG_OSAME_AURA_LIGHTEN, draw_aiharasuu,\
+    FONT_SIZE_OSAME_NUM, UC_DUST, UC_AURA
 from mod.huda import Huda
 from mod.ol.view_banmen import view_youso
 from mod.card import Card
@@ -25,20 +26,18 @@ class PlayHuyo():
         self.inject_func = delivery.inject_view
         self.button_dust = Button(img_nega=IMG_OSAME_DUST, img_lighten=IMG_OSAME_DUST_LIGHTEN, x=WX/2-110, y=WY/2-150)
         self.button_aura = Button(img_nega=IMG_OSAME_AURA, img_lighten=IMG_OSAME_AURA_LIGHTEN, x=WX/2+110, y=WY/2-150)
-        self.dust_num = 2
-        self.aura_num = 0
-        self._rearrange_button(button=self.button_dust, img_nega=IMG_OSAME_DUST, img_lighten=IMG_OSAME_DUST_LIGHTEN, num=self.dust_num)
-        self._rearrange_button(button=self.button_aura, img_nega=IMG_OSAME_AURA, img_lighten=IMG_OSAME_AURA_LIGHTEN, num=self.aura_num)
+        self.osame = card.osame(delivery, hoyuusya)
+        self.dust_num = delivery.ouka_count(hoyuusya=hoyuusya, is_mine=False, utuwa_code=UC_DUST)
+        self.aura_num = delivery.ouka_count(hoyuusya=hoyuusya, is_mine=True, utuwa_code=UC_AURA)
+        self.dust_osame = min(self.dust_num, self.osame)
+        self.aura_osame = min(self.aura_num, self.osame-self.dust_osame)
+        self._rearrange()
         self.button_decision = Button(img_nega=IMG_DECISION, img_lighten=IMG_DECISION_LIGHTEN)
         self.buttons = [self.button_dust, self.button_aura, self.button_decision]
 
-    def _rearrange_button(self, button: Button, img_nega: Surface, img_lighten: Surface, num: int) -> None:
-        button.img_nega = img_nega.copy()
-        draw_aiharasuu(surface=button.img_nega, dest=Vector2(button.img_nega.get_size())/2-
-                       [FONT_SIZE_OSAME_NUM/2, FONT_SIZE_OSAME_NUM/2], num=num, size=FONT_SIZE_OSAME_NUM)
-        button.img_lighten = img_lighten.copy()
-        draw_aiharasuu(surface=button.img_lighten, dest=Vector2(button.img_lighten.get_size())/2-
-                       [FONT_SIZE_OSAME_NUM/2, FONT_SIZE_OSAME_NUM/2], num=num, size=FONT_SIZE_OSAME_NUM)
+    def _rearrange(self) -> None:
+        _rearrange_button(button=self.button_dust, img_nega=IMG_OSAME_DUST, img_lighten=IMG_OSAME_DUST_LIGHTEN, num=self.dust_osame)
+        _rearrange_button(button=self.button_aura, img_nega=IMG_OSAME_AURA, img_lighten=IMG_OSAME_AURA_LIGHTEN, num=self.aura_osame)
 
     def elapse(self) -> None:
         screen.blit(source=self.card.img, dest=-Vector2(self.card.img.get_size())/2+Vector2(WX, WY)/2)
@@ -60,5 +59,15 @@ class PlayHuyo():
 
     def moderate(self, stat: Any) -> None:
         ...
+
+def _rearrange_button(button: Button, img_nega: Surface, img_lighten: Surface, num: int) -> None:
+    button.img_nega = _img_in_number(img_base=img_nega, num=num)
+    button.img_lighten = _img_in_number(img_base=img_lighten, num=num)
+
+def _img_in_number(img_base: Surface, num: int) -> Surface:
+    img_return = img_base.copy()
+    draw_aiharasuu(surface=img_return, dest=Vector2(img_return.get_size())/2-
+                    [FONT_SIZE_OSAME_NUM/2, FONT_SIZE_OSAME_NUM/2], num=num, size=FONT_SIZE_OSAME_NUM)
+    return img_return
 
 # compatible_with(, OverLayer)
