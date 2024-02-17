435b9b22208e9cb349ea9eddab329230055160d9
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-02-18 03:43:43 +0900

    J320:基本動作もHudaの単一選択に一般化したいのでKihonDousaクラスを創設

--- mod/kihondousa.py
+++ mod/kihondousa.py
@@ -1,6 +1,27 @@
-from mod.const import UC_MAAI, UC_AURA, UC_DUST, UC_FLAIR
+#                 20                  40                  60                 79
+from typing import Any
+from pygame.surface import Surface
+from mod.const import CT_HUTEI, UC_MAAI, UC_AURA, UC_DUST, UC_FLAIR
 from mod.delivery import Delivery
 from mod.popup_message import popup_message
+from mod.card import BoolDI, Card, KoukaDI, MaaiDI, SuuziDI, TaiounizeDI, auto_di, identity_di, int_di, pass_di, whole_di
+
+class KihonDousa(Card):
+    send_ouka: KoukaDI
+
+    def add_init(self, text: str, from_mine: bool, from_code: int, to_mine: bool, to_code: int) -> None:
+        self.cond: BoolDI = lambda delivery, hoyuusya: delivery.can_ouka_to_ryouiki(
+            hoyuusya=hoyuusya, from_mine=from_mine, from_code=from_code,
+            to_mine=to_mine, to_code=to_code)
+        def do(delivery: Delivery, hoyuusya: int) -> None:
+            popup_message.add(text)
+            delivery.send_ouka_to_ryouiki(
+                hoyuusya=hoyuusya, from_mine=from_mine, from_code=from_code,
+                to_mine=to_mine, to_code=to_code)
+        self.send_ouka = do
+
+    def kaiketu(self, delivery: Delivery, hoyuusya: int, huda: Any | None = None) -> None:
+        self.send_ouka(delivery, hoyuusya)
 
 def pass_koudou(delivery: Delivery, hoyuusya: int) -> None:
     pass
