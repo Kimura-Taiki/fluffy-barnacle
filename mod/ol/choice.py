5c2b3ca87194498eb9c550f04562ca9cc2f7f451
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-02-18 00:50:37 +0900

    J318:Choice.factoryをProxyTabaFactoryから生成

--- mod/ol/choice.py
+++ mod/ol/choice.py
@@ -5,12 +5,13 @@ from mod.const import screen, IMG_GRAY_LAYER, compatible_with, WX, WY, TC_SUTEHU
 from mod.huda import Huda
 from mod.ol.view_banmen import view_youso
 from mod.card import Card
-from mod.tf.taba_factory import TabaFactory
+# from mod.tf.taba_factory import TabaFactory
 from mod.controller import controller
 from mod.popup_message import popup_message
 from mod.moderator import moderator
 from mod.delivery import Delivery
 from mod.ol.pop_stat import PopStat
+from mod.ol.proxy_taba_factory import ProxyTabaFactory
 
 HAND_X: Callable[[int, int], float] = lambda i, j: WX/2-110*(j-1)+220*i
 HAND_Y: Callable[[int, int], float] = lambda i, j: WY/2-150
@@ -24,9 +25,7 @@ class Choice():
         self.source_huda = huda if isinstance(huda, Huda) else None
         self.name = "効果の選択"
         self.inject_func = delivery.inject_view
-        factory = TabaFactory(inject_kwargs={
-            "draw": Huda.available_draw, "hover": Huda.detail_draw, "mousedown": self._mousedown, "mouseup": self._mouseup
-            }, huda_x=HAND_X, huda_y=HAND_Y, huda_angle=HAND_ANGLE)
+        factory = ProxyTabaFactory(inject_kwargs={"mouseup": self._mouseup})
         self.taba = factory.maid_by_cards(cards=cards, hoyuusya=self.hoyuusya)
 
     def elapse(self) -> None:
@@ -44,9 +43,6 @@ class Choice():
 
     def moderate(self, stat: PopStat) -> None:
         ...
-
-    def _mousedown(self, huda: Huda) -> None:
-        controller.active = huda
 
     def _mouseup(self, huda: Huda) -> None:
         # popup_message.add(text="PlayKougeki.mouseup でクリック確定したよ")
