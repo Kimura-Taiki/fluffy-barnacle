717e2c8e486c5f91cdcda665a9363b1910ff6a9c
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-02-18 15:04:22 +0900

    J337:others_basic_actionからkaiketu_layer_factoryを省く

--- mod/ol/others_basic_action.py
+++ mod/ol/others_basic_action.py
@@ -10,13 +10,12 @@ from mod.ol.undo_mouse import make_undo_youso
 from mod.ol.proxy_taba_factory import ProxyTabaFactory
 from mod.card import Card
 from mod.ol.mc_layer_factory import MonoChoiceLayer
-from mod.ol.kaiketu_layer_facotry import kaiketu_layer_factory
 from mod.ol.pop_stat import PopStat
 
 _cards: list[Card] = [zensin_card, ridatu_card, koutai_card, matoi_card, yadosi_card]
 
 def _mouseup(huda: Huda) -> None:
-    moderator.append(_KihondousaKaiketu(huda=huda))
+    huda.card.kaiketu(huda.delivery, huda.hoyuusya)
 
 def _moderate(mcl: MonoChoiceLayer, stat: PopStat) -> None:
     mcl.delivery.send_huda_to_ryouiki(huda=mcl.source_huda, is_mine=True, taba_code=TC_HUSEHUDA)
@@ -29,8 +28,3 @@ def others_basic_action_layer(delivery: Delivery, hoyuusya: int, huda: Any | Non
     mcl.taba = factory.maid_by_cards(cards=_cards, hoyuusya=hoyuusya)
     mcl.other_hover = make_undo_youso(text="OthersBasicAction")
     return mcl
-
-def _dih(delivery: Delivery, hoyuusya: int, huda: Huda) -> None:
-    huda.card.kaiketu(delivery, hoyuusya)
-
-_KihondousaKaiketu = kaiketu_layer_factory(name="の基本動作", dih=_dih)
