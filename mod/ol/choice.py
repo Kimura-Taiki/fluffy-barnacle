7cd1ddd409a7f2f2b35caf3d588aed67358ba93b
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-02-18 14:53:07 +0900

    J335:partialを省く

--- mod/ol/choice.py
+++ mod/ol/choice.py
@@ -1,6 +1,5 @@
 #                 20                  40                  60                 79
 from typing import Any
-from functools import partial
 
 from mod.const import TC_SUTEHUDA
 from mod.huda import Huda
@@ -12,8 +11,8 @@ from mod.ol.mc_layer_factory import MonoChoiceLayer
 from mod.ol.kaiketu_layer_facotry import kaiketu_layer_factory
 from mod.ol.pop_stat import PopStat
 
-def _mouseup(huda: Huda, mcl: MonoChoiceLayer) -> None:
-    moderator.append(ChoiceKaiketu(huda=huda))
+def _mouseup(huda: Huda) -> None:
+    moderator.append(_ChoiceKaiketu(huda=huda))
 
 def _moderate(mcl: MonoChoiceLayer, stat: PopStat) -> None:
     if mcl.source_huda:
@@ -23,11 +22,11 @@ def _moderate(mcl: MonoChoiceLayer, stat: PopStat) -> None:
 def choice_layer(cards: list[Card], delivery: Delivery, hoyuusya: int, huda: Any | None=None) -> MonoChoiceLayer:
     mcl = MonoChoiceLayer(name="効果の選択", delivery=delivery, hoyuusya=hoyuusya, huda=huda,
                           moderate=_moderate)
-    factory = ProxyTabaFactory(inject_kwargs={"mouseup": partial(_mouseup, mcl=mcl)})
+    factory = ProxyTabaFactory(inject_kwargs={"mouseup": _mouseup})
     mcl.taba = factory.maid_by_cards(cards=cards, hoyuusya=hoyuusya)
     return mcl
 
 def _dih(delivery: Delivery, hoyuusya: int, huda: Huda) -> None:
     huda.card.kaiketu(delivery, hoyuusya)
 
-ChoiceKaiketu = kaiketu_layer_factory(name="を選択", dih=_dih)
+_ChoiceKaiketu = kaiketu_layer_factory(name="を選択", dih=_dih)
--- mod/ol/others_basic_action.py
+++ mod/ol/others_basic_action.py
@@ -1,6 +1,5 @@
 #                 20                  40                  60                 79
 from typing import Any
-from functools import partial
 
 from mod.const import TC_HUSEHUDA
 from mod.delivery import Delivery
@@ -16,8 +15,8 @@ from mod.ol.pop_stat import PopStat
 
 _cards: list[Card] = [zensin_card, ridatu_card, koutai_card, matoi_card, yadosi_card]
 
-def _mouseup(huda: Huda, mcl: MonoChoiceLayer) -> None:
-    moderator.append(KihondousaKaiketu(huda=huda))
+def _mouseup(huda: Huda) -> None:
+    moderator.append(_KihondousaKaiketu(huda=huda))
 
 def _moderate(mcl: MonoChoiceLayer, stat: PopStat) -> None:
     mcl.delivery.send_huda_to_ryouiki(huda=mcl.source_huda, is_mine=True, taba_code=TC_HUSEHUDA)
@@ -26,7 +25,7 @@ def _moderate(mcl: MonoChoiceLayer, stat: PopStat) -> None:
 def others_basic_action_layer(delivery: Delivery, hoyuusya: int, huda: Any | None=None) -> MonoChoiceLayer:
     mcl = MonoChoiceLayer(name="基本動作の選択", delivery=delivery, hoyuusya=hoyuusya, huda=huda,
                           moderate=_moderate)
-    factory = ProxyTabaFactory(inject_kwargs={"mouseup": partial(_mouseup, mcl=mcl)})
+    factory = ProxyTabaFactory(inject_kwargs={"mouseup": _mouseup})
     mcl.taba = factory.maid_by_cards(cards=_cards, hoyuusya=hoyuusya)
     mcl.other_hover = make_undo_youso(text="OthersBasicAction")
     return mcl
@@ -34,4 +33,4 @@ def others_basic_action_layer(delivery: Delivery, hoyuusya: int, huda: Any | Non
 def _dih(delivery: Delivery, hoyuusya: int, huda: Huda) -> None:
     huda.card.kaiketu(delivery, hoyuusya)
 
-KihondousaKaiketu = kaiketu_layer_factory(name="の基本動作", dih=_dih)
+_KihondousaKaiketu = kaiketu_layer_factory(name="の基本動作", dih=_dih)
