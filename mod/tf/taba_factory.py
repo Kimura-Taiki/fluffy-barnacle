a706cc95bd8be66e123bbcdb51e8df5f27333f74
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-02-19 13:00:22 +0900

    J344:taba_facotryの位置定数を削除

--- mod/tf/taba_factory.py
+++ mod/tf/taba_factory.py
@@ -8,15 +8,6 @@ from mod.huda import Huda
 from mod.taba import Taba
 from mod.card import Card
 
-HAND_X_RATE: Callable[[int], float] = lambda i: 120-130*max(0, i-4)/i
-HAND_X: Callable[[int, int], int | float] = lambda i, j: WX/2-HAND_X_RATE(j)/2*(j-1)+HAND_X_RATE(j)*i
-
-HAND_Y_DIFF: Callable[[int, int], float] = lambda i, j: abs(i*2-(j-1))*(1 if j < 3 else 3/(j-1))
-HAND_Y: Callable[[int, int], int | float] = lambda i, j: WY-60+HAND_Y_DIFF(i, j)**2*2
-
-HAND_ANGLE_RATE: Callable[[int], float] = lambda i: -6 if i < 3 else -6.0*3/(i-1)
-HAND_ANGLE: Callable[[int, int], int | float] = lambda i, j: -HAND_ANGLE_RATE(j)/2*(j-1)+HAND_ANGLE_RATE(j)*i
-
 class TabaFactory():
     def __init__(self, inject_kwargs: dict[str, Callable[[Huda], None]],
                  huda_x: Callable[[int, int], float], huda_y: Callable[[int, int], float],
@@ -38,7 +29,6 @@ class TabaFactory():
             huda = Huda(img=i)
             huda.hoyuusya = hoyuusya
             taba.append(huda)
-            # taba.append(Huda(img=i))
         return taba
 
     def maid_by_cards(self, cards: list[Card], hoyuusya: int) -> Taba:
