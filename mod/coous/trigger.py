6708f72a8d4a9de8ee3823277b4b1ba2ea122312
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-02-26 00:39:28 +0900

    J449:誘発効果を担うContinuousとしてTriggerクラスを創設

---
+++ mod/coous/trigger.py
@@ -0,0 +1,26 @@
+#                 20                  40                  60                 79
+from typing import Callable, Any, runtime_checkable, Protocol
+from copy import copy
+
+from mod.const import CF_TRIGGER
+from mod.delivery import Delivery
+from mod.coous.continuous import Continuous, BoolDII, auto_dii
+
+__all__ = ['BoolDII', 'auto_dii']
+
+@runtime_checkable
+class TriggerEffect(Protocol):
+    def kaiketu(self, delivery: Delivery, hoyuusya: int, huda: Any | None, code: int) -> None:
+        ...
+
+class Trigger(Continuous):
+    def __init__(self, name: str, cond: BoolDII, trigger: int, effect: TriggerEffect) -> None:
+        self.name = name
+        self.type = CF_TRIGGER
+        self.cond = cond
+        self.trigger = trigger
+        self.effect = effect
+
+    def __str__(self) -> str:
+        return f"Continuous{vars(self)}"
+
