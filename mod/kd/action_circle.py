2023b59f41ae05f5af83202a0d0df9bcc1291b6f
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-02-27 07:45:30 +0900

    J481:action_circleで他基本動作を選ぶ際に使用可能な基本動作のみ抽出する_available_basic_acitonsを作る

--- mod/kd/action_circle.py
+++ mod/kd/action_circle.py
@@ -5,7 +5,7 @@ from pygame.math import Vector2
 from mod.const import screen, ACTION_CIRCLE_NEUTRAL, ACTION_CIRCLE_YADOSI,\
     ACTION_CIRCLE_BASIC, ACTION_CIRCLE_ZENSIN, ACTION_CIRCLE_CARD, enforce,\
     OBAL_KIHONDOUSA, OBAL_SYUUTYUU, OBAL_USE_CARD
-from mod.classes import Callable, Card, Youso, Huda, popup_message, controller
+from mod.classes import Callable, Card, Youso, Huda, Delivery, popup_message, controller
 from mod.ol.others_basic_action import obal_func
 from mod.kd.kihondousa import zensin_card, ridatu_card, koutai_card, matoi_card, yadosi_card
 
@@ -36,12 +36,15 @@ def _use_card(card: Card) -> Callable[[Youso], None]:
 def _not_card(youso: Youso) -> None:
     popup_message.add("集中力はカードではありません")
 
+def _available_basic_actions(delivery: Delivery, hoyuusya: int) -> list[Card]:
+    return [card for card in [zensin_card, ridatu_card, koutai_card, matoi_card, yadosi_card] if card.cond(delivery, hoyuusya)]
+
 def mouseup(youso: Youso, mode: int=OBAL_KIHONDOUSA) -> None:
     diff_coord = pygame.mouse.get_pos()-controller.hold_coord
     if diff_coord.length_squared() < 50: return
     {3: _use_card(enforce(youso, Huda).card) if mode==OBAL_KIHONDOUSA else _not_card,
      2: obal_func(cards=[yadosi_card], name="標準行動：宿し", mode=mode),
-     1: obal_func(cards=[zensin_card, ridatu_card, koutai_card, matoi_card, yadosi_card], name="標準行動：その他基本動作", text="その他基本動作です", mode=mode)
+     1: obal_func(cards=_available_basic_actions(delivery=youso.delivery, hoyuusya=youso.hoyuusya), name="標準行動：その他基本動作", text="その他基本動作です", mode=mode)
      }.get(int((diff_coord.angle_to([0, 0])+225)/90),
            obal_func(cards=[zensin_card], name="標準行動：前進", mode=mode))(youso)
 
