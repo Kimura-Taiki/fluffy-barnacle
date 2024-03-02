b56efbf9350e958cd42a28411e0c6e7d7b1cad30
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-03-02 15:34:42 +0900

    J511:choice_layerからhuda引数を削除

--- mod/ol/choice.py
+++ mod/ol/choice.py
@@ -1,13 +1,11 @@
 #                 20                  40                  60                 79
-from typing import Any
-
 from mod.const import enforce, POP_OPEN, POP_CHOICED, POP_KAIKETUED
 from mod.classes import Card, Huda, Delivery, moderator
 from mod.ol.turns_progression.pipeline_layer import PipelineLayer
 from mod.ol.only_select_layer import OnlySelectLayer
 
-def choice_layer(cards: list[Card], delivery: Delivery, hoyuusya: int, huda:
-Any | None=None) -> PipelineLayer:
+def choice_layer(cards: list[Card], delivery: Delivery, hoyuusya: int) ->\
+PipelineLayer:
     return PipelineLayer(name="ChoiceLayer", delivery=delivery, gotoes={
         POP_OPEN: lambda l, s: moderator.append(OnlySelectLayer(delivery=
             delivery, hoyuusya=hoyuusya, name="効果の選択", lower=cards,
