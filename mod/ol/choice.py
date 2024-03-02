85cdd3a644c949631d8acd86a736ec0488437e44
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-03-02 15:31:45 +0900

    J510:choice_layerをOnlySelectLayer経由へ

--- mod/ol/choice.py
+++ mod/ol/choice.py
@@ -1,26 +1,18 @@
 #                 20                  40                  60                 79
 from typing import Any
 
-from mod.const import TC_SUTEHUDA
-from mod.huda.huda import Huda
-from mod.card.card import Card
-from mod.moderator import moderator
-from mod.delivery import Delivery
-from mod.tf.taba_factory import TabaFactory
-from mod.ol.mc_layer_factory import MonoChoiceLayer
-from mod.ol.pop_stat import PopStat
+from mod.const import enforce, POP_OPEN, POP_CHOICED, POP_KAIKETUED
+from mod.classes import Card, Huda, Delivery, moderator
+from mod.ol.turns_progression.pipeline_layer import PipelineLayer
+from mod.ol.only_select_layer import OnlySelectLayer
 
-def _mouseup(huda: Huda) -> None:
-    huda.card.kaiketu(huda.delivery, huda.hoyuusya)
-
-def _moderate(mcl: MonoChoiceLayer, stat: PopStat) -> None:
-    if mcl.source_huda:
-        mcl.delivery.send_huda_to_ryouiki(huda=mcl.source_huda, is_mine=True, taba_code=TC_SUTEHUDA)
-    moderator.pop()
-
-def choice_layer(cards: list[Card], delivery: Delivery, hoyuusya: int, huda: Any | None=None) -> MonoChoiceLayer:
-    mcl = MonoChoiceLayer(name="効果の選択", delivery=delivery, hoyuusya=hoyuusya, huda=huda,
-                          moderate=_moderate)
-    factory = TabaFactory(inject_kwargs={"mouseup": _mouseup}, is_ol=True)
-    mcl.taba = factory.maid_by_cards(cards=cards, hoyuusya=hoyuusya)
-    return mcl
+def choice_layer(cards: list[Card], delivery: Delivery, hoyuusya: int, huda:
+Any | None=None) -> PipelineLayer:
+    return PipelineLayer(name="ChoiceLayer", delivery=delivery, gotoes={
+        POP_OPEN: lambda l, s: moderator.append(OnlySelectLayer(delivery=
+            delivery, hoyuusya=hoyuusya, name="効果の選択", lower=cards,
+            code=POP_CHOICED)),
+        POP_CHOICED: lambda l, s: enforce(s.huda, Huda).card.kaiketu(delivery=
+            delivery, hoyuusya=hoyuusya, code=POP_KAIKETUED),
+        POP_KAIKETUED: lambda l, s: moderator.pop()
+    })
