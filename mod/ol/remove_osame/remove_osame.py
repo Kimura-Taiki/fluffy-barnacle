1d2218719fedffaeff1f697afc802c8fb85e4e94
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-03-03 22:40:19 +0900

    J541:PipelineLayer系列のremove_osame_layerを作成

--- mod/ol/remove_osame/remove_osame.py
+++ mod/ol/remove_osame/remove_osame.py
@@ -1,5 +1,6 @@
 #                 20                  40                  60                 79
-from mod.const import pass_func, enforce, POP_OK, POP_HUYO_ELAPSED
+from mod.const import pass_func, enforce, POP_OK, POP_OPEN, POP_SINGLE_ELAPSED\
+    , POP_HUYO_ELAPSED
 from mod.classes import Callable, Any, PopStat, Huda, Delivery, moderator, popup_message
 from mod.ol.remove_osame.single_remove import single_remove_layer, huyo_hudas
 from mod.ol.only_select_layer import OnlySelectLayer
@@ -33,7 +34,27 @@ class RemoveOsame():
         else:
             _append(delivery=self.delivery, hoyuusya=self.hoyuusya,
                     hudas=stat.rest_taba)
+            
+def _open(layer: PipelineLayer, stat: PopStat, code: int) -> None:
+    popup_message.add("付与の納を償却します")
+    layer.moderate(PopStat(code=code, rest_taba=huyo_hudas(delivery=
+        layer.delivery, hoyuusya=layer.hoyuusya)))
+    
+def _single_elapsed(layer: PipelineLayer, stat: PopStat, code: int) -> None:
+    if not stat.rest_taba:
+        moderator.pop()
+    else:
+        moderator.append(single_remove_layer(hudas=stat.rest_taba,
+            delivery=layer.delivery, hoyuusya=layer.hoyuusya, code=code))
 
 def _append(delivery: Delivery, hoyuusya: int, hudas: list[Huda]) -> None:
     moderator.append(single_remove_layer(hudas=hudas, delivery=delivery,
         hoyuusya=hoyuusya, code=POP_OK))
+
+                # 20                  40                  60                 79
+def remove_osame_layer(layer: PipelineLayer) -> PipelineLayer:
+    return PipelineLayer(name="付与の償却", delivery=layer.delivery, hoyuusya=
+        layer.hoyuusya, gotoes={
+POP_OPEN: lambda l, s: _open(l, s, POP_SINGLE_ELAPSED),
+POP_SINGLE_ELAPSED: lambda l, s: _single_elapsed(l, s, POP_SINGLE_ELAPSED),
+        }, code=POP_HUYO_ELAPSED)
