02cc96464b102e8af9ce89354b89a3fe2030154c
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-03-06 13:30:29 +0900

    J576:play_koudou_layerを作成

--- mod/ol/play_koudou.py
+++ mod/ol/play_koudou.py
@@ -1,13 +1,20 @@
 #                 20                  40                  60                 79
-# from typing import Any
-
-from mod.const import POP_OK
+from mod.const import enforce, POP_OK, POP_OPEN
 from mod.classes import Any, PopStat, Card, Huda, Delivery, moderator
-# from mod.huda.huda import Huda
-# from mod.card.card import Card
-# from mod.moderator import moderator
-# from mod.delivery import Delivery
-# from mod.ol.pop_stat import PopStat
+from mod.ol.pipeline_layer import PipelineLayer
+
+def _open(layer: PipelineLayer, stat: PopStat) -> None:
+    enforce(layer.card, Card).kouka(layer.delivery, layer.hoyuusya)
+    if moderator.last_layer() == layer:
+        moderator.pop()
+
+def play_koudou_layer(card: Card, delivery: Delivery, hoyuusya: int,
+                      huda: Any | None, code: int=POP_OK) -> PipelineLayer:
+    return PipelineLayer(name=f"行動:{card.name}の使用", delivery=delivery,
+        hoyuusya=hoyuusya, gotoes={
+POP_OPEN: _open,
+POP_OK: lambda l, s: moderator.pop()
+        }, card=card, huda=huda, code=code)
 
 class PlayKoudou():
     def __init__(self, card: Card, delivery: Delivery, hoyuusya: int, huda: Any | None, code: int=POP_OK) -> None:
