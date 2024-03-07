4c8c051b3340878ebae072eb9a22aa3f8fa9c3bd
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-03-08 03:21:26 +0900

    J597:huda_add_draw._maai_detailで修正された間合を表示する様に変更

--- mod/huda/huda_add_draw.py
+++ mod/huda/huda_add_draw.py
@@ -1,10 +1,13 @@
 #                 20                  40                  60                 79
+import pygame
 from pygame.surface import Surface
 from pygame.math import Vector2
 
 from mod.const import IMG_OSAME, draw_aiharasuu, USAGE_DEPLOYED, enforce,\
-    TC_TEHUDA, TC_KIRIHUDA, IMG_ATTACK_STAT, CT_KOUGEKI, CT_HUYO
+    TC_TEHUDA, TC_KIRIHUDA, IMG_ATTACK_STAT, CT_KOUGEKI, CT_HUYO,\
+    MS_MINCHO_COL, BLACK, FONT_SIZE_MAAI, WHITE
 from mod.classes import Huda, Taba
+from mod.card.card_func import maai_text
 
 def img_detail(huda: 'Huda') -> Surface:
     return {CT_KOUGEKI: _kougeki_detail, CT_HUYO: _huyo_detail}.get(
@@ -21,6 +24,12 @@ def _damage_detail(huda: 'Huda', img: Surface) -> Surface:
         draw_aiharasuu(surface=img, dest=Vector2(38, 405), num=ld)
     return img
 
+def _maai_detail(huda: 'Huda', img: Surface) -> Surface:
+    text = MS_MINCHO_COL(maai_text(huda.card.maai_list(huda.delivery, huda.hoyuusya)), FONT_SIZE_MAAI, BLACK)
+    pygame.draw.rect(img, WHITE, (45, 7, text.get_width(), text.get_height()), width=0)
+    img.blit(source=text, dest=[45, 7])
+    return img
+
 def _kougeki_detail(huda: 'Huda') -> Surface:
     if huda.delivery.is_duck():
         return _others_detail(huda=huda)
@@ -30,6 +39,7 @@ def _kougeki_detail(huda: 'Huda') -> Surface:
         return _others_detail(huda=huda)
     detail = huda.huda_draw.img_nega.copy()
     detail = _damage_detail(huda=huda, img=detail)
+    detail = _maai_detail(huda=huda, img=detail)
     # detail.blit(source=IMG_ATTACK_STAT, dest=[8, 385])
     # if (ad := huda.card.aura_damage(delivery=huda.delivery, hoyuusya=huda.hoyuusya)) is not None:
     #     draw_aiharasuu(surface=detail, dest=Vector2(-2, 375), num=ad)
