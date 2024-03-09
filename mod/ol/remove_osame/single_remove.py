22b77f59af0f8cf24bebecc0ba8a90868ed38ebe
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-03-10 05:55:41 +0900

    J628:single_removeのamortizeとhakiziを分離し、納破棄時の選択余地を残す

--- mod/ol/remove_osame/single_remove.py
+++ mod/ol/remove_osame/single_remove.py
@@ -2,7 +2,7 @@
 from itertools import product
 
 from mod.const import TC_SUTEHUDA, TC_KIRIHUDA, USAGE_DEPLOYED, USAGE_USED,\
-    POP_OK, POP_OPEN, POP_CHOICED, POP_KAIKETUED, enforce
+    POP_OK, POP_OPEN, POP_CHOICED, POP_ACT1, POP_KAIKETUED, enforce
 from mod.classes import PopStat, Huda, Taba, Delivery, moderator
 from mod.ol.only_select_layer import OnlySelectLayer
 from mod.ol.pipeline_layer import PipelineLayer
@@ -24,16 +24,22 @@ def _amortize_huyo(layer: PipelineLayer, stat: PopStat, code: int) -> None:
         return
     huda = enforce(stat.huda, Huda)
     base = huda.base
-    base.card.amortize(base)
-    # base.delivery.send_ouka_to_ryouiki(hoyuusya=base.hoyuusya, from_huda=base)
+    layer.huda = base
     layer.rest = [enforce(huda, Huda).base for huda in stat.rest_taba]
-    if base.osame == 0:
-        base.usage = USAGE_USED
-        if base.card.hakizi:
-            base.card.hakizi.kaiketu(delivery=base.delivery, hoyuusya=base.
+    base.card.amortize(base)
+    if moderator.last_layer() == layer:
+        layer.moderate(PopStat(code=code))
+
+def _hakizi(layer: PipelineLayer, stat: PopStat, code: int) -> None:
+    huda = enforce(layer.huda, Huda)
+    if huda.osame == 0:
+        huda.usage = USAGE_USED
+        if huda.card.hakizi:
+            huda.card.hakizi.kaiketu(delivery=huda.delivery, hoyuusya=huda.
                                      hoyuusya, code=code)
             return
     layer.moderate(PopStat(code=code))
+
 
 def single_remove_layer(hudas: list[Huda], delivery: Delivery, hoyuusya: int,
     code: int=POP_OK) -> PipelineLayer:
@@ -41,6 +47,7 @@ def single_remove_layer(hudas: list[Huda], delivery: Delivery, hoyuusya: int,
         POP_OPEN: lambda l, s: moderator.append(OnlySelectLayer(delivery=
             delivery, hoyuusya=hoyuusya, name="償却する付与の選択", lower=hudas,
             code=POP_CHOICED)),
-        POP_CHOICED: lambda l, s,: _amortize_huyo(l, s, POP_KAIKETUED),
+        POP_CHOICED: lambda l, s,: _amortize_huyo(l, s, POP_ACT1),
+        POP_ACT1: lambda l, s: _hakizi(l, s, POP_KAIKETUED),
         POP_KAIKETUED: lambda l, s: moderator.pop()
     }, code=code)
