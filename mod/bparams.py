5d6b52739918c83ac4555867562061a58d807ab7
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-03-10 07:01:32 +0900

    J630:BParamsのmaai, tatuzin_no_maai属性が可変になるように関数注入可能に

--- mod/bparams.py
+++ mod/bparams.py
@@ -1,8 +1,14 @@
+#                 20                  40                  60                 79
+from typing import Callable
+
 class BParams():
-    def __init__(self) -> None:
+    def __init__(self, maai_func: Callable[[], int] | None=None,
+                 tatuzin_func: Callable[[], int] | None=None) -> None:
         self.start_turn()
         self.turn_count = 1
-        self.tatuzin_no_maai = 2
+        self._maai_func = maai_func if maai_func else self._default_maai_func
+        self._tatuzin_no_maai = 2
+        self._tatuzin_func = tatuzin_func if tatuzin_func else self._default_tatuzin_func
         self.tehuda_max = 4
         self.damage_attr = 0
 
@@ -11,5 +17,19 @@ class BParams():
         self.during_taiou = False
         self.attack_megami = -1
 
+    @property
+    def tatuzin_no_maai(self) -> int:
+        return self._tatuzin_func()
+    
+    @property
+    def maai(self) -> int:
+        return self._maai_func()
+
     def __str__(self) -> str:
         return f"BParams{vars(self)}"
+    
+    def _default_tatuzin_func(self) -> int:
+        return self._tatuzin_no_maai
+    
+    def _default_maai_func(self) -> int:
+        return 0
