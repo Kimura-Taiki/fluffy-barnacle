8664a18436f683976d035a604a8a1aef02983f05
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-03-09 16:00:23 +0900

    J623:TempKoudou作成時にYazirusi材料でも作成可能に

--- mod/card/temp_koudou.py
+++ mod/card/temp_koudou.py
@@ -1,3 +1,4 @@
+#                 20                  40                  60                 79
 from pygame.surface import Surface
 from typing import Any
 
@@ -5,18 +6,48 @@ from mod.const import IMG_HAKUSI, MS_MINCHO_COL, BLACK, IMG_FT_ARROW, IMG_FT_OUK
     , IMG_FT_MAAI, IMG_FT_DUST, IMG_FT_ZYOGAI, IMG_FT_AI_AURA, IMG_FT_AI_FLAIR, IMG_FT_AI_LIFE, IMG_FT_AI_SYUUTYUU\
     , IMG_FT_ZI_AURA, IMG_FT_ZI_FLAIR, IMG_FT_ZI_LIFE, IMG_FT_ZI_SYUUTYUU, UC_MAAI, UC_DUST, UC_ZYOGAI\
     , UC_AURA, UC_FLAIR, UC_LIFE, UC_SYUUTYUU, CT_KOUDOU
+from mod.classes import Delivery
 from mod.card.card import Card, BoolDI, KoukaDI
+from mod.card.kw.yazirusi import Yazirusi
 
 class TempKoudou(Card):
-    def __init__(self, name: str, cond: BoolDI, kouka: KoukaDI, todo: list[list[Any]]=[]) -> None:
+    def __init__(self, name: str, cond: BoolDI, kouka: KoukaDI | None=None, todo: list[list[Any]]=[],
+                 yazirusi: Yazirusi | list[Yazirusi]=[]) -> None:
         self.img = IMG_HAKUSI.copy()
         self.y = 20
         self._draw_name(name=name)
+        if kouka:
+            self._maid_by_kouka(name=name, cond=cond, kouka=kouka, todo=todo)
+        elif yazirusi:
+            self._maid_by_yazirusi(name=name, cond=cond, yazirusi=yazirusi)
+        else:
+            raise ValueError("koukaかyazirusiは必要だよ")
+        # for i in todo:
+        #     if isinstance(i[0], bool):
+        #         self._draw_yazirusi(from_mine=i[0], from_code=i[1], to_mine=i[2], to_code=i[3], kazu=i[4])
+        #     elif isinstance(i[0], str):
+        #         self._draw_text(texts=i)
+        # super().__init__(self.img, name, cond, CT_KOUDOU, kouka=kouka)
+
+    def _maid_by_kouka(self, name: str, cond: BoolDI, kouka: KoukaDI, todo: list[list[Any]]=[]) -> None:
         for i in todo:
             if isinstance(i[0], bool):
                 self._draw_yazirusi(from_mine=i[0], from_code=i[1], to_mine=i[2], to_code=i[3], kazu=i[4])
             elif isinstance(i[0], str):
                 self._draw_text(texts=i)
+        super().__init__(self.img, name, cond, CT_KOUDOU, kouka=kouka)
+
+    def _maid_by_yazirusi(self, name: str, cond: BoolDI, yazirusi: Yazirusi | list[Yazirusi]) -> None:
+        if isinstance(yazirusi, Yazirusi):
+            li = [yazirusi]
+        else:
+            li = yazirusi
+        for yazirusi in li:
+            self._draw_yazirusi(from_mine=yazirusi.from_mine, from_code=yazirusi.from_code,
+                to_mine=yazirusi.to_mine, to_code=yazirusi.to_code, kazu=yazirusi.kazu)
+        def kouka(delivery: Delivery, hoyuusya: int) -> None:
+            for yazirusi in li:
+                yazirusi.send(delivery=delivery, hoyuusya=hoyuusya)
         super().__init__(self.img, name, cond, CT_KOUDOU, kouka=kouka)
 
     def _draw_name(self, name: str) -> None:
