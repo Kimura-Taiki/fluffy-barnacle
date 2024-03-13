de7a906f0e9e330e85ccfc0ee152facf5da3ad8c
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-03-14 02:01:25 +0900

    J681:設置を機動可能な枠を_settiに置く

--- kaiketus/m05o.py
+++ kaiketus/m05o.py
@@ -3,12 +3,12 @@ import pygame
 from pygame import Surface
 from copy import copy
 
-from mod.const import enforce, opponent, MG_TOKOYO, CT_KOUGEKI, CT_KOUDOU, CT_HUYO, CT_ZENRYOKU,\
+from mod.const import enforce, opponent, MG_OBORO, CT_KOUGEKI, CT_KOUDOU, CT_HUYO, CT_ZENRYOKU,\
     CT_TAIOU, UC_LIFE, IMG_BYTE, UC_MAAI, UC_ZYOGAI, UC_SYUUTYUU, TG_1_OR_MORE_DAMAGE, IMG_NO_CHOICE,\
     UC_AURA, UC_FLAIR, UC_DUST, SC_TATUZIN, POP_OPEN, POP_ACT1, POP_ACT2, POP_ACT3, TG_END_PHASE,\
     SC_SMOKE, TC_YAMAHUDA, TC_TEHUDA, TC_HUSEHUDA, TC_SUTEHUDA
 from mod.classes import Callable, Card, Huda, Delivery, moderator
-from mod.card.card import auto_di, int_di, dima_di, BoolDI, SuuziDI, BoolDIC
+from mod.card.card import auto_di, int_di, dima_di, BoolDI, SuuziDI, BoolDIC, nega_dic
 from mod.card.temp_koudou import TempKoudou
 from mod.coous.attack_correction import Attack, AttackCorrection, mine_cf, BoolDIIC, auto_diic
 from mod.ol.pop_stat import PopStat
@@ -31,4 +31,11 @@ from mod.card.kw.discard import discard_card
 _ADDRESS = "na_05_oboro"
 def img_card(add: str) ->  Surface:
     return pygame.image.load(f"cards/{_ADDRESS}_{add}.png")
+
+n_1 = Card(megami=MG_OBORO, img=img_card("o_n_1"), name="鋼糸", cond=auto_di, type=CT_KOUGEKI,
+    aura_damage_func=int_di(2), life_damage_func=int_di(2), maai_list=dima_di(3, 4), setti=True)
+
+n_2 = Card(megami=MG_OBORO, img=img_card("o_n_2_s2"), name="影菱", cond=auto_di, type=CT_KOUGEKI,
+    aura_damage_func=int_di(2), life_damage_func=int_di(1), maai_list=dima_di(2, 2), taiouble=nega_dic,
+    setti=True)
 
--- mod/card/kw/saikousei.py
+++ mod/card/kw/saikousei.py
@@ -1,10 +1,11 @@
 #                 20                  40                  60                 79
 from random import shuffle
 
-from mod.const import TC_YAMAHUDA, TC_HUSEHUDA, TC_SUTEHUDA, USAGE_DEPLOYED,\
-    enforce
-from mod.classes import Huda, Taba, Delivery
+from mod.const import enforce, TC_YAMAHUDA, TC_HUSEHUDA, TC_SUTEHUDA,\
+    USAGE_DEPLOYED, POP_OPEN, POP_ACT1, POP_ACT2, POP_ACT3, POP_ACT4
+from mod.classes import PopStat, Huda, Taba, Delivery, moderator
 from mod.card.temp_koudou import TempKoudou, auto_di
+from mod.ol.pipeline_layer import PipelineLayer
 
 def _saikousei_hudas(delivery: Delivery, hoyuusya: int) -> list[Huda]:
     taba1, taba2, taba3 = [enforce(delivery.taba_target(hoyuusya=hoyuusya,
@@ -15,9 +16,20 @@ def _saikousei_hudas(delivery: Delivery, hoyuusya: int) -> list[Huda]:
     shuffle(moto)
     return moto
 
+def _setti(layer: PipelineLayer, stat: PopStat, code: int) -> None:
+    layer.moderate(PopStat(code))
+
+def _make_yamahuda(layer: PipelineLayer, stat: PopStat, code: int) -> None:
+    for huda in _saikousei_hudas(layer.delivery, layer.hoyuusya):
+        layer.delivery.send_huda_to_ryouiki(huda=huda, is_mine=True,
+                                            taba_code=TC_YAMAHUDA)
+    layer.moderate(PopStat(code))
+
 def saikousei(delivery: Delivery, hoyuusya: int) -> None:
-    for huda in _saikousei_hudas(delivery=delivery, hoyuusya=hoyuusya):
-        delivery.send_huda_to_ryouiki(huda=huda, is_mine=True, taba_code=
-                                      TC_YAMAHUDA)
+    moderator.append(PipelineLayer("再構成", delivery, hoyuusya, gotoes={
+        POP_OPEN: lambda l, s: _setti(l, s, POP_ACT1),
+        POP_ACT1: lambda l, s: _make_yamahuda(l, s, POP_ACT2),
+        POP_ACT2: lambda l, s: moderator.pop()
+    }))
 
 saikousei_card = TempKoudou(name="再構成", cond=auto_di, kouka=saikousei)
