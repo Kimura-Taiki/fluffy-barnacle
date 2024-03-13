67f1dfe18bb35b32b8514c1e0d6e6adbbac3ed27
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-03-13 22:51:53 +0900

    J676:千歳の鳥の為に再構成処理を分離

---
+++ mod/card/kw/saikousei.py
@@ -0,0 +1,20 @@
+#                 20                  40                  60                 79
+from random import shuffle
+
+from mod.const import TC_YAMAHUDA, TC_HUSEHUDA, TC_SUTEHUDA, USAGE_DEPLOYED,\
+    enforce
+from mod.classes import Huda, Taba, Delivery
+
+def _saikousei_hudas(delivery: Delivery, hoyuusya: int) -> list[Huda]:
+    taba1, taba2, taba3 = [enforce(delivery.taba_target(hoyuusya=hoyuusya,
+        is_mine=True, taba_code=taba_code), Taba) for taba_code in
+        [TC_YAMAHUDA, TC_HUSEHUDA, TC_SUTEHUDA]]
+    moto = list(taba1)+list(taba2)+[
+        huda for huda in taba3 if huda.usage!= USAGE_DEPLOYED]
+    shuffle(moto)
+    return moto
+
+def saikousei(delivery: Delivery, hoyuusya: int) -> None:
+    for huda in _saikousei_hudas(delivery=delivery, hoyuusya=hoyuusya):
+        delivery.send_huda_to_ryouiki(huda=huda, is_mine=True, taba_code=
+                                      TC_YAMAHUDA)
