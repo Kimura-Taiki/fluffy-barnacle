0e9dd2acfb230ad76e10adafdb6ce3725459c812
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-03-14 02:36:06 +0900

    J682:設置の草稿を書き上げる

--- mod/card/kw/saikousei.py
+++ mod/card/kw/saikousei.py
@@ -1,11 +1,13 @@
 #                 20                  40                  60                 79
 from random import shuffle
 
-from mod.const import enforce, TC_YAMAHUDA, TC_HUSEHUDA, TC_SUTEHUDA,\
-    USAGE_DEPLOYED, POP_OPEN, POP_ACT1, POP_ACT2, POP_ACT3, POP_ACT4
-from mod.classes import PopStat, Huda, Taba, Delivery, moderator
+from mod.const import enforce, side_name, IMG_NO_CHOICE, CT_KOUDOU, TC_YAMAHUDA,\
+    TC_HUSEHUDA, TC_SUTEHUDA, USAGE_DEPLOYED, POP_OPEN, POP_ACT1, POP_ACT2, POP_ACT3, POP_ACT4
+from mod.classes import PopStat, Card, Huda, Taba, Delivery, moderator
 from mod.card.temp_koudou import TempKoudou, auto_di
 from mod.ol.pipeline_layer import PipelineLayer
+from mod.ol.only_select_layer import OnlySelectLayer
+from mod.ol.turns_progression.setti import setti_layer
 
 def _saikousei_hudas(delivery: Delivery, hoyuusya: int) -> list[Huda]:
     taba1, taba2, taba3 = [enforce(delivery.taba_target(hoyuusya=hoyuusya,
@@ -16,9 +18,6 @@ def _saikousei_hudas(delivery: Delivery, hoyuusya: int) -> list[Huda]:
     shuffle(moto)
     return moto
 
-def _setti(layer: PipelineLayer, stat: PopStat, code: int) -> None:
-    layer.moderate(PopStat(code))
-
 def _make_yamahuda(layer: PipelineLayer, stat: PopStat, code: int) -> None:
     for huda in _saikousei_hudas(layer.delivery, layer.hoyuusya):
         layer.delivery.send_huda_to_ryouiki(huda=huda, is_mine=True,
@@ -27,7 +26,7 @@ def _make_yamahuda(layer: PipelineLayer, stat: PopStat, code: int) -> None:
 
 def saikousei(delivery: Delivery, hoyuusya: int) -> None:
     moderator.append(PipelineLayer("再構成", delivery, hoyuusya, gotoes={
-        POP_OPEN: lambda l, s: _setti(l, s, POP_ACT1),
+        POP_OPEN: lambda l, s: moderator.append(setti_layer(l, s, POP_ACT1)),
         POP_ACT1: lambda l, s: _make_yamahuda(l, s, POP_ACT2),
         POP_ACT2: lambda l, s: moderator.pop()
     }))
---
+++ mod/ol/turns_progression/setti.py
@@ -0,0 +1,38 @@
+#                 20                  40                  60                 79
+from mod.const import enforce, side_name, OBAL_USE_CARD, IMG_NO_CHOICE, CT_KOUDOU, TC_YAMAHUDA,\
+    TC_HUSEHUDA, TC_SUTEHUDA, USAGE_DEPLOYED, POP_OPEN, POP_ACT1, POP_ACT2, POP_ACT3, POP_ACT4
+from mod.classes import PopStat, Card, Huda, Delivery, moderator
+from mod.card.temp_koudou import TempKoudou, auto_di
+from mod.ol.pipeline_layer import PipelineLayer
+from mod.ol.only_select_layer import OnlySelectLayer
+from mod.ol.use_card_layer import use_card_layer
+
+_no_choice = Card(img=IMG_NO_CHOICE, name="何もしない", cond=auto_di, type=CT_KOUDOU)
+
+def _setti_hudas(delivery: Delivery, hoyuusya: int) -> list[Huda]:
+    li: list[Huda] = []
+    for huda in delivery.taba_target(hoyuusya, True, TC_HUSEHUDA):
+        if isinstance(huda, Huda) and "setti" in huda.card.kwargs and\
+        huda.card.can_play(delivery, hoyuusya):
+            li.append(huda)
+    return li
+
+def _choice_setti(layer: PipelineLayer, stat: PopStat, code: int) -> None:
+    moderator.append(OnlySelectLayer(layer.delivery, layer.hoyuusya, "起動する設置を選択",
+        lower=_setti_hudas(layer.delivery, layer.hoyuusya), upper=[_no_choice], code=code))
+
+def _use_setti(layer: PipelineLayer, stat: PopStat, code: int) -> None:
+    huda = enforce(stat.huda, Huda).base
+    if huda.card.name == "何もしない":
+        layer.moderate(PopStat(code))
+        return
+    name = f"設置された{side_name(layer.hoyuusya)}の「{huda.card.name}」を使います"
+    moderator.append(use_card_layer(cards=[huda.card], name=name, youso=huda,
+                                    mode=OBAL_USE_CARD, code=code))
+
+def setti_layer(layer: PipelineLayer, stat: PopStat, code: int) -> PipelineLayer:
+    return PipelineLayer("設置", layer.delivery, layer.hoyuusya, gotoes={
+        POP_OPEN: lambda l, s: _choice_setti(l, s, POP_ACT1),
+        POP_ACT1: lambda l, s: _use_setti(l, s, POP_ACT2),
+        POP_ACT2: lambda l, s: moderator.pop()
+    }, code=code)
