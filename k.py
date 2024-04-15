9b938e52eacb00739a5e312b23caada6fe967299
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-04-15 16:38:17 +0900

    K002:restra枝に自動上書きされるか確認

--- k.py
+++ k.py
@@ -3,9 +3,19 @@ import pygame
 from mod.const.screen import screen, clock, FRAMES_PER_SECOND, IMG_YATUBA_BG
 from mod.controller import controller
 
+class Viewer():
+    def __init__(self) -> None:
+        pass
+
+    def draw(self) -> None:
+        screen.blit(source=IMG_YATUBA_BG, dest=[0, 0])
+        ...
+
+viewer = Viewer()
+
 def mainloop() -> None:
     controller.resolve_pygame_events()
-    screen.blit(source=IMG_YATUBA_BG, dest=[0, 0])
+    viewer.draw()
     pygame.display.update()
     clock.tick(FRAMES_PER_SECOND)
 
