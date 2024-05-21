966ac6281731a6358d83357dab587ceea32789b9
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-05-21 21:24:19 +0900

    L008:LogSquareに描画も委任

--- ll/mod/log_square.py
+++ ll/mod/log_square.py
@@ -1,54 +1,32 @@
 from pygame import Rect, Color, Surface, SRCALPHA, transform, image
 from copy import deepcopy
 
-from mod.screen import screen
-from mod.func import rect_fill
-from mod.font import MS_MINCHO_COL
+from mod.func import ratio_rect
 from mod.kard import Kard
 from ptc.square import Square
 from ptc.player import Player
 from ptc.element import Element
 
 class LogSquare():
-    def __init__(self, kard: Kard, rect: Rect) -> None:
-        self.kard = kard
-        self.rect = self._rect(rect=rect)
+    _RATIO = (136, 190)
 
-class PlayerSquare():
-    def __init__(self, player: Player, rect: Rect) -> None:
-        self.player = player
-        self.rect = self._rect(rect=rect)
+    def __init__(self, kard: Kard, rect: Rect, canvas: Surface) -> None:
+        self.kard = kard
+        self.rect = ratio_rect(rect=rect, ratio=self._RATIO)
         self.img = self._img()
+        self.canvas = canvas
 
     def get_hover(self) -> Element | None:
         return None
 
     def draw(self) -> None:
-        screen.blit(source=self.img, dest=self.rect)
-        ...
-        # rect_fill(color=self.translucent_color, rect=self.rect)
-        # screen.blit(source=MS_MINCHO_COL(self.player.name, 24, "black"), dest=self.rect)
-        # for i, kard in enumerate(self.player.log):
-        #     dd
-
-    @property
-    def translucent_color(self) -> Color:
-        color = deepcopy(self.player.color)
-        color.a = int(color.a/2)
-        return color
-
-    def _rect(self, rect: Rect) -> Rect:
-        w, h = (420*rect.h/288, rect.h) if rect.w*288 > rect.h*420 else (rect.w, 288*rect.w/420)
-        return Rect(rect.left+(rect.w-w)/2, rect.top+(rect.h-h)/2, w, h)
+        self.canvas.blit(source=self.img, dest=self.rect)
 
     def _img(self) -> Surface:
-        img = Surface(size=(420, 288), flags=SRCALPHA)
-        rect_fill(color=self.translucent_color, rect=Rect(0, 0, 420, 288), surface=img)
-        img.blit(source=MS_MINCHO_COL(self.player.name, 24, "black"), dest=(0, 0))
-        for i, kard in enumerate(self.player.log):
-            img.blit(
-                source=transform.rotozoom(
-                    surface=image.load(f"ll/pic/{kard.png_file}").convert_alpha(),
-                    angle=0.0, scale=0.4),
-                dest=(10+i*80, 70))
-        return transform.rotozoom(surface=img, angle=0.0, scale=self.rect.w/420)
+        img = Surface(size=self._RATIO, flags=SRCALPHA)
+        img.blit(
+            source=transform.rotozoom(
+                surface=image.load(f"ll/pic/{self.kard.png_file}").convert_alpha(),
+                angle=0.0, scale=0.4),
+            dest=(0, 0))
+        return transform.rotozoom(surface=img, angle=0.0, scale=self.rect.w/self._RATIO[0])
--- ll/mod/player_square.py
+++ ll/mod/player_square.py
@@ -5,24 +5,32 @@ from mod.screen import screen
 from mod.func import rect_fill, ratio_rect
 from mod.font import MS_MINCHO_COL
 from mod.kard import Kard
+from mod.log_square import LogSquare
 from ptc.square import Square
 from ptc.player import Player
 from ptc.element import Element
 
 class PlayerSquare():
     _RATIO = (420, 288)
+    _LOG_RATIO = (136, 190)
 
     def __init__(self, player: Player, rect: Rect) -> None:
         self.player = player
         self.rect = ratio_rect(rect=rect, ratio=self._RATIO)
-        self.rect = self._rect(rect=rect)
         self.img = self._img()
+        self.log_squares = [LogSquare(
+            kard=kard,
+            rect=Rect((10+i*80, 70), self._LOG_RATIO),
+            canvas=self.img
+        ) for i, kard in enumerate(self.player.log)]
 
     def get_hover(self) -> Element | None:
         return None
 
     def draw(self) -> None:
         screen.blit(source=self.img, dest=self.rect)
+        for log_square in self.log_squares:
+            log_square.draw()
 
     @property
     def translucent_color(self) -> Color:
@@ -30,18 +38,8 @@ class PlayerSquare():
         color.a = int(color.a/2)
         return color
 
-    def _rect(self, rect: Rect) -> Rect:
-        w, h = (420*rect.h/288, rect.h) if rect.w*288 > rect.h*420 else (rect.w, 288*rect.w/420)
-        return Rect(rect.left+(rect.w-w)/2, rect.top+(rect.h-h)/2, w, h)
-
     def _img(self) -> Surface:
         img = Surface(size=self._RATIO, flags=SRCALPHA)
         rect_fill(color=self.translucent_color, rect=Rect(0, 0, 420, 288), surface=img)
         img.blit(source=MS_MINCHO_COL(self.player.name, 24, "black"), dest=(0, 0))
-        for i, kard in enumerate(self.player.log):
-            img.blit(
-                source=transform.rotozoom(
-                    surface=image.load(f"ll/pic/{kard.png_file}").convert_alpha(),
-                    angle=0.0, scale=0.4),
-                dest=(10+i*80, 70))
         return transform.rotozoom(surface=img, angle=0.0, scale=self.rect.w/420)
