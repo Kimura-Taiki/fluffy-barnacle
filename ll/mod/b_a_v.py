17fce31f4136ab9f7f36e38863d192f84c272e4c
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-05-21 18:09:55 +0900

    L006:ログを描画

--- ll/mod/b_a_v.py
+++ ll/mod/b_a_v.py
@@ -7,7 +7,7 @@ from ptc.player import Player
 def _make_board() -> Board:
     players: list[Player] = [
         ManPlayer(name="you", color="crimson", log=[]),
-        ManPlayer(name="Seiji", color="darkgreen", log=[_kard1]),
+        ManPlayer(name="Seiji", color="darkgreen", log=[_kard1, _kard1, _kard1]),
         ManPlayer(name="William", color="purple", log=[]),
         ManPlayer(name="Gastone", color="gold", log=[])]
     return Board(players=players)
--- ll/mod/player_square.py
+++ ll/mod/player_square.py
@@ -1,9 +1,10 @@
-from pygame import Rect, Color, Surface, SRCALPHA, transform
+from pygame import Rect, Color, Surface, SRCALPHA, transform, image
 from copy import deepcopy
 
 from mod.screen import screen
 from mod.func import rect_fill
 from mod.font import MS_MINCHO_COL
+from mod.kard import Kard
 from ptc.square import Square
 from ptc.player import Player
 from ptc.element import Element
@@ -39,6 +40,10 @@ class PlayerSquare():
         img = Surface(size=(420, 288), flags=SRCALPHA)
         rect_fill(color=self.translucent_color, rect=Rect(0, 0, 420, 288), surface=img)
         img.blit(source=MS_MINCHO_COL(self.player.name, 24, "black"), dest=(0, 0))
-        # rect_fill(color=self.translucent_color, rect=self.rect, surface=img)
-        # img.blit(source=MS_MINCHO_COL(self.player.name, 24, "black"), dest=self.rect)
+        for i, kard in enumerate(self.player.log):
+            img.blit(
+                source=transform.rotozoom(
+                    surface=image.load(f"ll/pic/{kard.png_file}").convert_alpha(),
+                    angle=0.0, scale=0.4),
+                dest=(10+i*80, 70))
         return transform.rotozoom(surface=img, angle=0.0, scale=self.rect.w/420)
