ddadfae52e87710229999808171c7379c9338e9e
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-05-26 19:16:18 +0900

    L043:timer_functionsの諸関数をmainloopで活用

--- ll/any/timer_functions.py
+++ ll/any/timer_functions.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame import Rect
 from time import time
 from typing import Callable
 
@@ -7,23 +8,29 @@ from any.font import MS_MINCHO_COL
 from any.screen import screen, WX, FRAMES_PER_SECOND
 
 _FONT_SIZE = 36
+_DEST = (WX-380, 300)
+_SIZE = (380, 40)
 
-def timer_functions() -> tuple[Callable[[], None], Callable[[], None]]:
+def timer_functions() -> tuple[Callable[[], None], Callable[[], None], Callable[[], int]]:
     from time import time
     log = 0.0
     times = [0.01]*FRAMES_PER_SECOND
+    frame_count = 0
     def start_timer() -> None:
-        nonlocal log
+        nonlocal log, frame_count
         log = time()
+        frame_count += 1
     def end_timer() -> None:
         nonlocal log, times
         elapsed_time = time()-log
         times.append(elapsed_time)
         times.pop(0)
-        rect_fill(color="lemonchiffon", rect=[WX-340, 240, WX, 40])
+        rect_fill(color="lemonchiffon", rect=Rect((_DEST), (_SIZE)))
         # pygame.draw.rect(surface=screen, color="lemonchiffon", rect=[WX-340, 240, WX, 40], width=0)
         screen.blit(source=MS_MINCHO_COL(
             f"{(sum(times)/FRAMES_PER_SECOND*1000):.2f}ms/Loop, now{round(elapsed_time*1000, 2):.2f}", _FONT_SIZE, "black"),
-            dest=[WX-340, 240])
-    return start_timer, end_timer
-start_timer, end_timer = timer_functions()
+            dest=_DEST)
+    def frames() -> int:
+        return frame_count
+    return start_timer, end_timer, frames
+start_timer, end_timer, frames = timer_functions()
--- ll/ll.py
+++ ll/ll.py
@@ -2,13 +2,24 @@ import pygame
 
 from any.screen import clock, FRAMES_PER_SECOND
 from any.propagation import propagation
+from any.timer_functions import start_timer, end_timer, frames
 from seed.default_gm import gm
 from ptc.view import View
 
 def mainloop(view: View) -> None:
+    start_timer()
+
     propagation.resolve_pygame_events(get_hover=view.get_hover())
     view.draw()
+    from any.screen import screen, WX, WY
+    from any.font import MS_MINCHO_COL
+    screen.blit(
+        source=MS_MINCHO_COL(f"{frames()}frames", 64, "black"),
+        dest=(WX/2, WY/2)
+    )
     propagation.mouse_over()
+
+    end_timer()
     pygame.display.update()
     clock.tick(FRAMES_PER_SECOND)
 
