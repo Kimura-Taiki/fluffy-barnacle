9e4041bc9c18c3452c639201509d56b46e140454
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-05-26 17:58:52 +0900

    L036:DeckSquareの画像をany.pictures経由に

--- ll/any/pictures.py
+++ ll/any/pictures.py
@@ -6,7 +6,3 @@ def picload(file: str) -> Surface:
 IMG_BACK = picload("back")
 IMG_REST = picload("rest")
 IMG_NUM = picload("num")
-
-# IMG_BACK = image.load("ll/pic/back.png").convert_alpha()
-# IMG_REST = image.load("ll/pic/rest.png").convert_alpha()
-# IMG_NUM = image.load("ll/pic/num.png").convert_alpha()
--- ll/view/deck_square.py
+++ ll/view/deck_square.py
@@ -1,16 +1,14 @@
 from pygame import Rect, Color, Surface, SRCALPHA, transform, image
 
-from any.screen import screen
+from any.pictures import IMG_BACK, IMG_REST, IMG_NUM
 from any.func import rect_fill, ratio_rect, translucented_color
+from any.screen import screen
 from model.kard import Kard
 from ptc.element import Element
 
 from ptc.square import Square
 class DeckSquare():
     _RATIO = (360, 495)
-    _IMG_BACK = image.load("ll/pic/back.png").convert_alpha()
-    _IMG_REST = image.load("ll/pic/rest.png").convert_alpha()
-    _IMG_NUM = image.load("ll/pic/num.png").convert_alpha()
 
     def __init__(self, deck: list[Kard], rect: Rect) -> None:
         self.deck = deck
@@ -30,9 +28,9 @@ class DeckSquare():
     def _img(self) -> Surface:
         img = Surface(size=self._RATIO, flags=SRCALPHA)
         rect_fill(color=translucented_color(Color("coral")), rect=Rect((0, 0), self._RATIO), surface=img)
-        img.blit(source=self._IMG_BACK, dest=(10, 10))
-        img.blit(source=self._IMG_REST, dest=(90, 20))
-        img.blit(source=self._IMG_NUM, dest=(210, 20), area=Rect(self.count%10*60, self.count//10*60, 60, 60))
+        img.blit(source=IMG_BACK, dest=(10, 10))
+        img.blit(source=IMG_REST, dest=(90, 20))
+        img.blit(source=IMG_NUM, dest=(210, 20), area=Rect(self.count%10*60, self.count//10*60, 60, 60))
         return transform.rotozoom(surface=img, angle=0.0, scale=self.rect.w/self._RATIO[0])
 
     @property
