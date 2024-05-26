f6a4cdfc7cdd09965eab32e869ca979abcf785d6
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-05-26 18:34:29 +0900

    L039:PlayerSquare.mousedownを上位のBoardView側で定義

--- ll/view/board_view.py
+++ ll/view/board_view.py
@@ -1,4 +1,5 @@
-from pygame import Rect, Vector2 as V2
+from pygame import Rect, Surface, Vector2 as V2
+from typing import Callable
 
 from any.screen import screen, WX, WY
 from any.pictures import IMG_BG
@@ -7,6 +8,7 @@ from model.board import Board
 from model.player import Player
 from view.player_square import PlayerSquare
 from view.deck_square import DeckSquare
+from view.draw_view import DrawView
 from ptc.element import Element
 from ptc.square import Square
 from ptc.listener import Listener
@@ -41,25 +43,47 @@ class BoardView():
 
     def _squares(self) -> list[Square]:
         opponents = [player for player in self.board.players if player != self.subject]
-        # w = WX/len(opponents)
-        # h = WY*2/5
-        # d = WX*3/16
         ds = DeckSquare(deck=self.board.deck, rect=Rect(0, WY-_H, _D, _H))
         deck_square: list[Square] = [ds]
-        opponents_squares = self._opponents_squares(opponents=opponents)
-        # opponents_squares: list[Square] = [
-        #     PlayerSquare(player=player, rect=Rect(w*i, 0, w, h), listener=self.listener,)
-        #     for i, player in enumerate(opponents)]
+        opponents_squares = self._opponents_squares(opponents=opponents, ds=ds)
         subject_square: list[Square] = [
             PlayerSquare(player=self.subject, rect=Rect(_D, WY-_H, WX/4, _H), listener=self.listener,)
             ] if self.subject in self.board.players else []
         return opponents_squares+subject_square+deck_square
 
-    def _opponents_squares(self, opponents: list[Player]) -> list[Square]:
+    def _opponents_squares(self, opponents: list[Player], ds: DeckSquare) -> list[Square]:
         w = WX/len(opponents)
-        li: list[Square] = [PlayerSquare(
+        pss = [PlayerSquare(
             player=player,
             rect=Rect(w*i, 0, w, _H),
             listener=self.listener,)
             for i, player in enumerate(opponents)]
+        for ps in pss:
+            ps.mousedown = self._mousedown(
+                deck_square=ds,
+                player_square=ps)
+        li: list[Square] = [square for square in pss]
         return li
+
+    def _mousedown(self, deck_square: DeckSquare, player_square: PlayerSquare) -> Callable[[], None]:
+        def func() -> None:
+            self.listener.view = DrawView(
+                view=self.listener.view,
+                img_back=deck_square.img_back,
+                from_v2=V2(deck_square.rect.center),
+                to_v2=V2(player_square.rect.center),
+                callback=self._callback
+            )
+        return func
+
+    def _callback(self) -> None:
+        raise EOFError("ヨシ！")
+    # def _mousedown(self) -> None:
+    #     self.listener.view = DrawView(
+    #         view=self.listener.view,
+    #         from_v2=self.deck_v2,
+    #         to_v2=V2(self.rect.center),
+    #         callback=self._callback)
+
+    # def _callback(self) -> None:
+    #     raise EOFError("ヨシっ！")
