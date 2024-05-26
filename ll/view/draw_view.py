98710d234f8ac07eac13e752aab804f22e60c2cb
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-05-26 15:42:15 +0900

    L032:DrawViewの引数をview単独化

--- ll/view/draw_view.py
+++ ll/view/draw_view.py
@@ -1,22 +1,12 @@
-from pygame import Rect
-
-from any.screen import screen, IMG_BG, WX, WY
+from any.screen import screen, WX, WY
 from any.propagation import propagation
 from any.font import MS_MINCHO_COL
-from model.board import Board
-from model.player import Player
-from view.player_square import PlayerSquare
-from view.deck_square import DeckSquare
-from view.board_view import BoardView
 from ptc.element import Element
-from ptc.square import Square
-from ptc.listener import Listener
 
 from ptc.view import View
 class DrawView():
-    def __init__(self, board: Board, subject: Player, listener: Listener) -> None:
-        self.board_view = BoardView(board=board, subject=subject, listener=listener)
-        self.listener = listener
+    def __init__(self, view: View) -> None:
+        self.board_view = view
 
     def rearrange(self) -> None:
         ...
