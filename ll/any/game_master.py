667958316e14c501fc14b446d1be2ab03884ed04
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-05-26 21:20:02 +0900

    L046:mainloopをGameMasterの命令に転換

--- ll/any/game_master.py
+++ ll/any/game_master.py
@@ -1,3 +1,8 @@
+import pygame
+
+from any.timer_functions import start_timer, end_timer
+from any.propagation import propagation
+from any.screen import clock, FRAMES_PER_SECOND
 from model.board import Board
 from ptc.view import View, EMPTY_VIEW
 from ptc.notification import Notification
@@ -10,3 +15,22 @@ class GameMaster():
 
     def listen(self, nf: Notification) -> None:
         self.board = nf.created_board(board=self.board)
+
+    def mainloop(self) -> None:
+        start_timer()
+
+        propagation.resolve_pygame_events(get_hover=self.view.get_hover())
+        self.view.draw()
+        # from any.timer_functions import frames
+        # from any.screen import screen, WX, WY
+        # from any.font import MS_MINCHO_COL
+        # screen.blit(
+        #     source=MS_MINCHO_COL(f"{frames()}frames", 64, "black"),
+        #     dest=(WX/2, WY/2)
+        # )
+        propagation.mouse_over()
+
+        end_timer()
+        pygame.display.update()
+        clock.tick(FRAMES_PER_SECOND)
+
--- ll/ll.py
+++ ll/ll.py
@@ -6,23 +6,24 @@ from any.timer_functions import start_timer, end_timer
 from seed.default_gm import gm
 from ptc.view import View
 
-def mainloop(view: View) -> None:
-    start_timer()
+# def mainloop(view: View) -> None:
+#     start_timer()
 
-    propagation.resolve_pygame_events(get_hover=view.get_hover())
-    view.draw()
-    # from any.timer_functions import frames
-    # from any.screen import screen, WX, WY
-    # from any.font import MS_MINCHO_COL
-    # screen.blit(
-    #     source=MS_MINCHO_COL(f"{frames()}frames", 64, "black"),
-    #     dest=(WX/2, WY/2)
-    # )
-    propagation.mouse_over()
+#     propagation.resolve_pygame_events(get_hover=view.get_hover())
+#     view.draw()
+#     # from any.timer_functions import frames
+#     # from any.screen import screen, WX, WY
+#     # from any.font import MS_MINCHO_COL
+#     # screen.blit(
+#     #     source=MS_MINCHO_COL(f"{frames()}frames", 64, "black"),
+#     #     dest=(WX/2, WY/2)
+#     # )
+#     propagation.mouse_over()
 
-    end_timer()
-    pygame.display.update()
-    clock.tick(FRAMES_PER_SECOND)
+#     end_timer()
+#     pygame.display.update()
+#     clock.tick(FRAMES_PER_SECOND)
 
 while True:
-    mainloop(view=gm.view)
+    gm.mainloop()
+    # mainloop(view=gm.view)
