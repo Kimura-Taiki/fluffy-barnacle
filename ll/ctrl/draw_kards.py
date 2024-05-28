c657ca2c3ba605b4580552d74956f03425c37344
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-05-28 21:00:23 +0900

    L078:OldDrawKardsController依存をDrawKardsControllerから排除

--- ll/ctrl/draw_kards.py
+++ ll/ctrl/draw_kards.py
@@ -1,7 +1,7 @@
 from pygame import Surface, Vector2 as V2
 from typing import Callable
 
-from ctrl.old_draw_kards import OldDrawKardsController
+# from ctrl.old_draw_kards import OldDrawKardsController
 from model.player import Player
 from model.kard import EMPTY_KARD
 from ptc.bridge import Bridge
@@ -30,28 +30,28 @@ class DrawKardsController():
         # self.pq = pq if pq else self._player_square()
         self.to_v2 = V2((pq if pq else self._player_square()).rect.center)
 
-    def action(self) -> None:
-        OldDrawKardsController(
-            bridge=self.board_view.bridge,
-            # img_back=self.dq.img_back,
-            # from_v2=V2(self.dq.rect.center),
-            # to_v2=V2(self.pq.rect.center),
-            img_back=self.img_back,
-            from_v2=self.from_v2,
-            to_v2=self.to_v2,
-            player=self.player,
-            suffix=self.suffix
-        ).action()
-
     # def action(self) -> None:
-    #     self._old_view = self.bridge.view
-    #     self.bridge.view = LinearView(
-    #         view=self._old_view,
+    #     OldDrawKardsController(
+    #         bridge=self.board_view.bridge,
+    #         # img_back=self.dq.img_back,
+    #         # from_v2=V2(self.dq.rect.center),
+    #         # to_v2=V2(self.pq.rect.center),
     #         img_back=self.img_back,
     #         from_v2=self.from_v2,
     #         to_v2=self.to_v2,
-    #         callback=self._callback
-    #     )
+    #         player=self.player,
+    #         suffix=self.suffix
+    #     ).action()
+
+    def action(self) -> None:
+        self._old_view = self.bridge.view
+        self.bridge.view = LinearView(
+            view=self._old_view,
+            img_back=self.img_back,
+            from_v2=self.from_v2,
+            to_v2=self.to_v2,
+            callback=self._callback
+        )
 
     def _player_square(self) -> PlayerSquare:
         if ps := next((
@@ -65,6 +65,21 @@ class DrawKardsController():
                 f"該当プレイヤーはいません\nplayer.name = {self.player.name}"
             )
 
+    def _callback(self) -> None:
+        deck = self.bridge.board.deck
+        draw_kard = deck.pop(0)
+        if self.player.hand == EMPTY_KARD:
+            players=[player._replace(hand=draw_kard)
+                        if player.name == self.player.name else player
+                        for player in self.bridge.board.players]
+            self.bridge.board = self.bridge.board._replace(
+                deck=deck,
+                players=players
+            )
+        else:
+            self.bridge.board = self.bridge.board._replace(deck=deck, draw_kard=draw_kard)
+        self.bridge.view = self._old_view
+        self.suffix()
 
 
     # def __init__(
