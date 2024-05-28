2a928a33caf2587c62c1459c4185e9ec11d4936d
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-05-28 09:30:59 +0900

    L072:新生DrawKardsControllerの粗方を作成

--- ll/ctrl/draw_kards.py
+++ ll/ctrl/draw_kards.py
@@ -1,45 +1,80 @@
 from pygame import Surface, Vector2 as V2
 from typing import Callable
 
+from ctrl.old_draw_kards import OldDrawKardsController
 from model.player import Player
 from model.kard import EMPTY_KARD
 from ptc.bridge import Bridge
+from view.board_view import BoardView
+from view.deck_square import DeckSquare
 from view.linear_view import LinearView
+from view.player_square import PlayerSquare
 
 from ptc.controller import Controller
 class DrawKardsController():
-    def __init__(
-            self, bridge: Bridge, img_back: Surface, from_v2: V2, to_v2: V2, player: Player, suffix: Callable[..., None]
-    ) -> None:
-        self.bridge = bridge
-        self.img_back = img_back
-        self.from_v2 = from_v2
-        self.to_v2 = to_v2
+    def __init__(self, board_view: BoardView, player: Player, suffix: Callable[..., None]) -> None:
+        self.board_view = board_view
         self.player = player
         self.suffix = suffix
+        self.pq = self._player_square()
+        self.dq = board_view.deck_square
 
     def action(self) -> None:
-        self._old_view = self.bridge.view
-        self.bridge.view = LinearView(
-            view=self._old_view,
-            img_back=self.img_back,
-            from_v2=self.from_v2,
-            to_v2=self.to_v2,
-            callback=self._callback
-        )
+        return OldDrawKardsController(
+            bridge=self.board_view.bridge,
+            img_back=self.dq.img_back,
+            from_v2=V2(self.dq.rect.center),
+            to_v2=V2(self.pq.rect.center),
+            player=self.player,
+            suffix=self.suffix
+        ).action
 
-    def _callback(self) -> None:
-        deck = self.bridge.board.deck
-        draw_kard = deck.pop(0)
-        if self.player.hand == EMPTY_KARD:
-            players=[player._replace(hand=draw_kard)
-                        if player.name == self.player.name else player
-                        for player in self.bridge.board.players]
-            self.bridge.board = self.bridge.board._replace(
-                deck=deck,
-                players=players
-            )
+    def _player_square(self) -> PlayerSquare:
+        if ps := next((
+            square for square in self.board_view.squares if
+            isinstance(square, PlayerSquare) and
+            square.player.name == self.player.name
+        ), None):
+            return ps
         else:
-            self.bridge.board = self.bridge.board._replace(deck=deck, draw_kard=draw_kard)
-        self.bridge.view = self._old_view
-        self.suffix()
+            raise ValueError(
+                f"該当プレイヤーはいません\nplayer.name = {self.player.name}"
+            )
+
+
+
+    # def __init__(
+    #         self, bridge: Bridge, img_back: Surface, from_v2: V2, to_v2: V2, player: Player, suffix: Callable[..., None]
+    # ) -> None:
+    #     self.bridge = bridge
+    #     self.img_back = img_back
+    #     self.from_v2 = from_v2
+    #     self.to_v2 = to_v2
+    #     self.player = player
+    #     self.suffix = suffix
+
+    # def action(self) -> None:
+    #     self._old_view = self.bridge.view
+    #     self.bridge.view = LinearView(
+    #         view=self._old_view,
+    #         img_back=self.img_back,
+    #         from_v2=self.from_v2,
+    #         to_v2=self.to_v2,
+    #         callback=self._callback
+    #     )
+
+    # def _callback(self) -> None:
+    #     deck = self.bridge.board.deck
+    #     draw_kard = deck.pop(0)
+    #     if self.player.hand == EMPTY_KARD:
+    #         players=[player._replace(hand=draw_kard)
+    #                     if player.name == self.player.name else player
+    #                     for player in self.bridge.board.players]
+    #         self.bridge.board = self.bridge.board._replace(
+    #             deck=deck,
+    #             players=players
+    #         )
+    #     else:
+    #         self.bridge.board = self.bridge.board._replace(deck=deck, draw_kard=draw_kard)
+    #     self.bridge.view = self._old_view
+    #     self.suffix()
