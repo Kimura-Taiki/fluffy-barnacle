9b66349efee015002f02494b5f08533a0a47ed93
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-05-31 14:55:50 +0900

    L111:TurnStartsControllerのターン開始時描画をMessagesController経由に

--- ll/ctrl/turn_starts.py
+++ ll/ctrl/turn_starts.py
@@ -1,8 +1,8 @@
 from any.font import MS_MINCHO_COL
 from ctrl.draw_kards import DrawKardsController
+from ctrl.messages import MessagesController
 from ptc.bridge import Bridge
 from view.board_view import BoardView
-from view.message_view import MessageView
 
 _FONT_H = 96
 
@@ -12,18 +12,13 @@ class TurnStartsController():
         self.bridge = bridge
 
     def action(self) -> None:
-        view = self.bridge.view
-        if not isinstance(view, BoardView):
-            raise ValueError("TurnStartsControllerを起動する時はBoardViewでないと")
-        self._old_view = self.bridge.view
-        self.bridge.view = MessageView(
-            board_view=view,
+        MessagesController(
+            bridge=self.bridge,
             img_mes=MS_MINCHO_COL(f"{self.bridge.board.turn_player.name}'s Turn", _FONT_H, "black"),
-            callback=self._draw_callback
-        )
+            suffix=self._suffix
+        ).action()
 
-    def _draw_callback(self) -> None:
-        self.bridge.view = self._old_view
+    def _suffix(self) -> None:
         view = self.bridge.view
         if not isinstance(view, BoardView):
             raise ValueError("TurnStartsControllerを起動する時はBoardViewでないと")
@@ -31,8 +26,4 @@ class TurnStartsController():
             bridge=self.bridge,
             board_view=view,
             player=self.bridge.board.turn_player,
-            suffix=self._suffix
         ).action()
-
-    def _suffix(self) -> None:
-        ...
