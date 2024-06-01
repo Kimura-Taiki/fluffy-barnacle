423a156546ffa98b96f3ca3f437ec4e6d6145701
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-06-01 12:51:26 +0900

    L114:コメントアウトを削除

--- ll/ctrl/messages.py
+++ ll/ctrl/messages.py
@@ -9,7 +9,6 @@ from ptc.controller import Controller
 class MessagesController():
     def __init__(self, bridge: Bridge, img_mes: Surface, suffix: Callable[[], None]=lambda : None) -> None:
         self.bridge = bridge
-        # print("MessagesController.__init__", self.bridge.view)
         if not isinstance(self.bridge.view, BoardView):
             raise ValueError("MessagesController を起動する時はBoardViewでないと", self.bridge.view)
         self._old_view = self.bridge.view
@@ -17,7 +16,6 @@ class MessagesController():
         self.suffix = suffix
 
     def action(self) -> None:
-        # print("MessageController.action", self.bridge.view)
         self.bridge.view = MessageView(
             board_view=self._old_view,
             img_mes=self.img_mes,
@@ -25,7 +23,5 @@ class MessagesController():
         )
 
     def _draw_callback(self) -> None:
-        # print("MessageController._draw_callback", self.bridge.view)
         self.bridge.view = self._old_view
         self.suffix()
-        # print("MessageController._suffix.end", self.bridge.view)
--- ll/ctrl/use_kards.py
+++ ll/ctrl/use_kards.py
@@ -1,11 +1,8 @@
 from any.font import MS_MINCHO_COL
-from ctrl.draw_kards import DrawKardsController
 from ctrl.messages import MessagesController
 from ctrl.turn_starts import TurnStartsController
 from model.kard import Kard
 from ptc.bridge import Bridge
-from view.board_view import BoardView
-from view.message_view import MessageView
 from view.use_kard_view import UseKardView
 
 _FONT = 28
@@ -13,12 +10,10 @@ _FONT = 28
 from ptc.controller import Controller
 class UseKardsController():
     def __init__(self, bridge: Bridge, kard: Kard) -> None:
-        # print("UseKardsController.__init__")
         self.bridge = bridge
         self.kard = kard
 
     def action(self) -> None:
-        # print("UseKardsController.action")
         self.bridge.board.use_kard(player=self.bridge.board.turn_player, kard=self.kard)
         self._old_view = self.bridge.view
         self.bridge.view = UseKardView(
@@ -29,14 +24,11 @@ class UseKardsController():
 
     def _callback(self) -> None:
         self.bridge.view = self._old_view
-        # print("UseKardsController._callback", self.bridge.view)
         coroutines = [
             self._use_banpei, self._use_heisi, self._use_douke, self._use_kisi, self._use_souryo,
             self._use_mazyutusi, self._use_syougun, self._use_daizin, self._use_hime
         ]
         coroutines[self.kard.rank]()
-        # self.bridge.board.advance_to_next_turn()
-        # TurnStartsController(bridge=self.bridge).action()
 
     def _advance_to_next_turn(self) -> None:
         self.bridge.board.advance_to_next_turn()
@@ -79,33 +71,4 @@ class UseKardsController():
             bridge=self.bridge,
             img_mes=MS_MINCHO_COL(f"「姫」を捨てたので{self.bridge.board.turn_player.name}は脱落します", _FONT, "black"),
             suffix=self._advance_to_next_turn
-            # suffix=self._suffix
         ).action()
-
-    # def _suffix(self) -> None:
-    #     print("UseKardsController._suffix", self.bridge.view)
-    #     print("カード「姫」を使ったよ")
-
-    # def action(self) -> None:
-    #     handless_player = next((
-    #         player for player in self.bridge.board.players if len(player.hands) == 0
-    #     ), None)
-    #     view = self.bridge.view
-    #     if not isinstance(view, BoardView):
-    #         raise ValueError("SetupsControllerを起動する時はBoardViewでないと")
-    #     if handless_player:
-    #         DrawKardsController(
-    #             bridge=self.bridge,
-    #             board_view=view,
-    #             player=handless_player,
-    #             suffix=self._draw_suffix
-    #         ).action()
-    #     else:
-    #         self._game_start()
-
-    # def _draw_suffix(self) -> None:
-    #     self.action()
-
-    # def _game_start(self) -> None:
-    #     self.bridge.board.game_start()
-    #     TurnStartsController(bridge=self.bridge).action()
