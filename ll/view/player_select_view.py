efd021582c8acb8b7066dca165628e0b3c45f20f
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-06-03 00:41:09 +0900

    L132:PlayerSelectViewで選択時に光る様に

--- ll/view/player_select_view.py
+++ ll/view/player_select_view.py
@@ -1,10 +1,10 @@
 from pygame import Rect
-from copy import deepcopy
 from copy import copy
 
 from any.func import enforce, rect_fill, translucented_color
-from any.screen import screen, WX, WY
 from any.pictures import IMG_BG
+from any.screen import screen, WX, WY, FRAMES_PER_SECOND
+from any.timer_functions import frames
 from model.player import Player, OBSERVER
 from model.ui_element import UIElement
 from ptc.bridge import Bridge
@@ -12,7 +12,9 @@ from ptc.square import Square
 from view.board_view import BoardView
 from view.player_square import PlayerSquare
 
-# from ptc.view import View
+_SECONDS = 0.5
+_WAIT = int(FRAMES_PER_SECOND*_SECONDS)
+
 from ptc.transition import Transition
 class PlayerSelectView:
     def __init__(self, bridge: Bridge, exclude: Player=OBSERVER) -> None:
@@ -21,6 +23,8 @@ class PlayerSelectView:
         self.board_view = enforce(bridge.view, BoardView)
         self.player_squares = self._player_squares()
         self.other_squares = self._other_squares()
+        self.selected_player: Player | None = None
+        self.frames = frames()
 
     def rearrange(self) -> None:
         """レイアウトの再配置を行います"""
@@ -50,15 +54,26 @@ class PlayerSelectView:
         ...
 
     def in_progress(self) -> bool:
-        return True
+        return bool(not self.selected_player)
 
     def _player_squares(self) -> list[PlayerSquare]:
         li: list[PlayerSquare] = []
         for square in self.board_view.squares:
-            if isinstance(square, PlayerSquare) and square.player !=self.exclude:
-                pq = copy(square)
-                li.append(pq)
+            if isinstance(square, PlayerSquare) and square.player != self.exclude:
+                li.append(self._player_square(pq=square))
         return li
+
+    def _player_square(self, pq: PlayerSquare) -> PlayerSquare:
+        def hover(pq: PlayerSquare) -> None:
+            time = frames()-self.frames
+            rect_fill(
+                color=translucented_color(color="white"),
+                rect=pq.rect
+            )
+        copy_pq = copy(pq)
+        copy_pq.ui_element = UIElement(hover=lambda : hover(pq=copy_pq))
+        return copy_pq
+
 
     def _other_squares(self) -> list[Square]:
         li: list[Square] = []
