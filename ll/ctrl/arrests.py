e823b73c8b526ffadb5739c81e6cf93b4b21f429
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-06-09 02:50:11 +0900

    L180:ArrestsControllerを作成、Kardをdataclassに変更

---
+++ ll/ctrl/arrests.py
@@ -0,0 +1,42 @@
+from pygame import Rect
+
+from any.screen import screen
+from model.player import Player
+from ptc.bridge import Bridge
+from view.duel.duel_engage_transition import DuelEngageTransition
+from view.duel.duel_open_transition import DuelOpenTransition
+from view.duel.duel_slash_transition import DuelSlashTransition
+from view.moves_view import MovesView
+
+class ArrestsController():
+    def __init__(self, bridge: Bridge) -> None:
+        self.bridge = bridge
+
+    def action(self, p1: Player, p2: Player) -> None:
+        self.bridge.whileloop(new_view=MovesView(
+            view=self.bridge.view,
+            transitions=[DuelEngageTransition(
+                rect=Rect(200, 120, 880, 475),
+                p1=p1,
+                p2=p2,
+                canvas=screen
+            )]
+        ))
+        self.bridge.whileloop(new_view=MovesView(
+            view=self.bridge.view,
+            transitions=[DuelOpenTransition(
+                rect=Rect(200, 120, 880, 475),
+                p1=p1,
+                p2=p2,
+                canvas=screen
+            )]
+        ))
+        self.bridge.whileloop(new_view=MovesView(
+            view=self.bridge.view,
+            transitions=[DuelSlashTransition(
+                rect=Rect(200, 120, 880, 475),
+                p1=p1,
+                p2=p2,
+                canvas=screen
+            )]
+        ))
--- ll/model/kard.py
+++ ll/model/kard.py
@@ -1,12 +1,18 @@
 from pygame import Surface
-from typing import NamedTuple
+from dataclasses import dataclass
 
 from any.pictures import picload
 
-class Kard(NamedTuple):
+@dataclass(frozen=True)
+class Kard():
     name: str
     rank: int
     png_file: str
 
     def picture(self) -> Surface:
         return picload(self.png_file)
+    
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Kard):
+            return False
+        return self.name == other.name and self.rank == other.rank
