3273af66e1b607450b824a84765725aa7ad05674
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-06-10 05:24:42 +0900

    L186:bind_mv関数を作成

--- ll/ctrl/arrests.py
+++ ll/ctrl/arrests.py
@@ -6,12 +6,7 @@ from model.player import Player
 from ptc.bridge import Bridge
 from view.arrest.engage_mv import engage_mv
 from view.arrest.face_up_mv import face_up_mv
-# from view.linear_transition import LinearTransition
-# from view.transition.zoom_transition import ZoomTransitions
-# from view.arrest.engage_transition import EngageTransition
-# from view.arrest.open_transition import OpenTransition
-# from view.arrest.slash_transition import SlashTransition
-from view.moves_view import MovesView
+from view.arrest.bind_mv import bind_mv
 
 _Rect = Rect(200, 120, 880, 480)
 _Huda = V2(340, 475)
@@ -33,23 +28,9 @@ class ArrestsController():
             kard=kard,
             img_after=pk.picture() if pk == kard else IMG_HAZURE
         ))
-
-
-        # self.bridge.whileloop(new_view=MovesView(
-        #     view=self.bridge.view,
-        #     transitions=[DuelOpenTransition(
-        #         rect=Rect(200, 120, 880, 475),
-        #         p1=p1,
-        #         p2=p2,
-        #         canvas=screen
-        #     )]
-        # ))
-        # self.bridge.whileloop(new_view=MovesView(
-        #     view=self.bridge.view,
-        #     transitions=[DuelSlashTransition(
-        #         rect=Rect(200, 120, 880, 475),
-        #         p1=p1,
-        #         p2=p2,
-        #         canvas=screen
-        #     )]
-        # ))
+        self.bridge.whileloop(new_view=bind_mv(
+            bridge=self.bridge,
+            rect=_Rect,
+            kard=kard,
+            img_actor=pk.picture()
+        ))
---
+++ ll/view/arrest/bind_transition.py
@@ -0,0 +1,38 @@
+from pygame import Surface, Vector2 as V2
+
+from any.pictures import IMG_CHAIN_L, IMG_CHAIN_R
+from view.progress_helper import ProgressHelper
+
+_SECONDS = 0.5
+_CV2 = V2(340, 270)
+
+from ptc.transition import Transition
+class BindTransition():
+    def __init__(
+            self, img_actor: Surface, center: V2, canvas: Surface, seconds: float=_SECONDS
+    ) -> None:
+        self._ratio, self.in_progress, _, _, _, self.elapse\
+            = ProgressHelper(seconds=seconds).provide_progress_funcs()
+        self.get_hover = ProgressHelper.empty_get_hover
+        self.img_actor = img_actor
+        self.topleft = center-V2(img_actor.get_size())/2
+        self.canvas = canvas
+
+    def rearrange(self) -> None:
+        ...
+
+    def draw(self) -> None:
+        self.canvas.blit(source=self._img(), dest=self.topleft)
+
+    def _img(self) -> Surface:
+        img = self.img_actor.copy()
+        params: list[tuple[Surface, V2, V2]] = [
+            (IMG_CHAIN_L, -_CV2, V2()),
+            (IMG_CHAIN_R, V2(_CV2.x, -_CV2.y), V2())
+        ]
+        for source, from_v2, to_v2 in params:
+            img.blit(
+                source=source,
+                dest=from_v2.lerp(to_v2, self._ratio())
+            )
+        return img
