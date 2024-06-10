61a96bec23cf0a347c0e847875dcc68ad00836aa
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-06-10 15:55:05 +0900

    L199:z3.pyにYamlを試しに導入

--- z3.py
+++ z3.py
@@ -1,16 +1,29 @@
-from dataclasses import dataclass
+import yaml
+from typing import Any
 
-@dataclass
-class Kard:
-    name: str
-    rank: int
+# YAMLファイルの読み込み
+def yml_load() -> dict[str, dict[str, str]]:
+    with open('messages.yml', 'r') as file:
+        messages: dict[str, dict[str, str]] = yaml.safe_load(file)
+    return messages
 
-@dataclass
-class Player:
-    name: str
-    hands: list[Kard]
-    alive: bool
+messages = yml_load()
+print(messages)
 
-@dataclass
-class Board:
-    players: list[Player]
+def get_message(language: str, message_key: str, **kwargs: str) -> str:
+    template = messages.get(language, {}).get(message_key, '')
+    return template.format(**kwargs)
+
+# 例として日本語のメッセージを取得
+kard_name = "カード名"
+player_name = "プレイヤー名"
+message = get_message('jp', 'elimination_message', kard_name=kard_name, player_name=player_name)
+print(message)
+
+# 例として英語のメッセージを取得
+message = get_message('en', 'elimination_message', kard_name=kard_name, player_name=player_name)
+print(message)
+
+# 例として韓国語のメッセージを取得
+message = get_message('kr', 'elimination_message', kard_name=kard_name, player_name=player_name)
+print(message)
