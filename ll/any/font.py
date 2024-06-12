8cccf80b7ed4bd981fa687b3eed9c141cfb0b9db
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-06-13 02:57:25 +0900

    L226: YAML:board:fontにフォントを入れて動的にフォントが変わる様に指定

--- ll/any/font.py
+++ ll/any/font.py
@@ -2,9 +2,18 @@ from pygame import Surface, Color
 from pygame.font import Font
 from typing import Callable, Sequence
 
+from any.locales import lomes
+
 RGBAOutput = tuple[int, int, int, int]
 ColorValue = Color | int | str | tuple[int, int, int] | RGBAOutput | Sequence[int]
 
 # MS_MINCHO_COL: Callable[[str, int, ColorValue], Surface] = lambda s, i, c: Font("msmincho001.ttf", i).render(s, True, c)
 # MS_MINCHO_COL: Callable[[str, int, ColorValue], Surface] = lambda s, i, c: Font("ll/font/msmincho001.ttf", i).render(s, True, c)
-MS_MINCHO_COL: Callable[[str, int, ColorValue], Surface] = lambda s, i, c: Font("ll/font/ChosunGs.TTF", i).render(s, True, c)
+# MS_MINCHO_COL: Callable[[str, int, ColorValue], Surface] = lambda s, i, c: Font("ll/font/ChosunGs.TTF", i).render(s, True, c)
+MS_MINCHO_COL: Callable[[str, int, ColorValue], Surface] = lambda s, i, c: Font(
+    f"ll/font/{lomes(folder="board", key="font")}", i
+).render(s, True, c)
+
+# LL_RENDER: Callable[[str, int, ColorValue], Surface] = lambda s, i, c: Font(
+#     f"ll/font/{lomes(folder="board", key="font")}", i
+# ).render(s, True, c)
