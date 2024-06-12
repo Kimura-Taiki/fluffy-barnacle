063166a47c5ca56341854e75ec1fc6222422edfb
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-06-12 13:47:27 +0900

    L210:Kardプロトコルの振る舞いを属性からプロパティ・命令・関数に変更

--- ll/model/kard.py
+++ ll/model/kard.py
@@ -3,14 +3,23 @@ from typing import Protocol, runtime_checkable, Callable, Any
 
 @runtime_checkable
 class Kard(Protocol):
-    png_file: str
-    picture: Callable[[], Surface]
-    name: str
-    rank: int
-    use_func: Callable[[Any, Any], None]
     '''
     use_func(Bridge, Player) -> None
 
     カードを使用した際に起動する命令です。
     第１引数にBridge,第２引数にPlayerを代入してください。
     '''
+
+    def picture(self) -> Surface:
+        ...
+
+    def use_func(self, bridge: Any, player: Any) -> None:
+        ...
+
+    @property
+    def name(self) -> str:
+        ...
+
+    @property
+    def rank(self) -> int:
+        ...
