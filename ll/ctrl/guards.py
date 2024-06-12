0ebd01a96d533217724466dd33ba1719746f7560
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2024-06-12 21:06:16 +0900

    L220:YAML:souryo:guardsが広域で使われるのでYAML:board:guardsへ移管

--- ll/ctrl/guards.py
+++ ll/ctrl/guards.py
@@ -1,5 +1,4 @@
 from any.locales import lomes
-from model.kard import Kard
 from ptc.bridge import Bridge
 from view.message_view import MessageView
 
@@ -7,13 +6,8 @@ class GuardsController():
     def __init__(self, bridge: Bridge) -> None:
         self.bridge = bridge
 
-    # def action(self, kard: Kard) -> None:
-    #     self.bridge.whileloop(new_view=MessageView(
-    #         view=self.bridge.view,
-    #         img_mes=lomes(folder="kard", key="guards", kard_name=kard.name)
-    #     ))
     def action(self, kard_name: str) -> None:
         self.bridge.whileloop(new_view=MessageView(
             view=self.bridge.view,
-            img_mes=lomes(folder="kard", key="guards", kard_name=kard_name)
+            img_mes=lomes(folder="board", key="guards", kard_name=kard_name)
         ))
